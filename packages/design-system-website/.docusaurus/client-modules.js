025620c4bc0e5c833b8ddb73399fd1c2b908dca7
mu29 <hero@101.inc>
2022-06-21 23:45:53 +0900

    chore: docusaurus 빌드 결과를 .gitignore에 추가한다

--- packages/design-system-website/.docusaurus/client-modules.js
+++
@@ -1,7 +0,0 @@
-export default [
-  require('/Users/seosanghui/project/opensource/node_modules/infima/dist/css/default/default.css'),
-  require('/Users/seosanghui/project/opensource/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
-  require('/Users/seosanghui/project/opensource/node_modules/@docusaurus/theme-classic/lib/admonitions.css'),
-  require('/Users/seosanghui/project/opensource/node_modules/@docusaurus/theme-classic/lib/nprogress'),
-  require('/Users/seosanghui/project/opensource/packages/design-system-website/src/css/custom.css'),
-];
