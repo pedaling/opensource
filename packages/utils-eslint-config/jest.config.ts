4df9026018a4a3dc6727e5ddb11706b89afe6540
peter <peter@101.inc>
2022-06-22 02:10:39 +0900

    fix: utils-eslint-config jest를 수정한다

--- packages/utils-eslint-config/jest.config.ts
+++ packages/utils-eslint-config/jest.config.ts
@@ -1,10 +1,10 @@
 /* eslint-disable */
 export default {
   displayName: "shared-utils-eslint-config",
-  preset: "../../../jest.preset.js",
+  preset: "../../jest.preset.js",
   transform: {
     "^.+\\.[tj]s$": "babel-jest",
   },
   moduleFileExtensions: ["ts", "js", "html"],
-  coverageDirectory: "../../../coverage/libs/shared/utils-eslint-config",
+  coverageDirectory: "../../coverage/packages/utils-eslint-config",
 };
