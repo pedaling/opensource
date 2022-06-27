5c4a43f3f61a59c3bf031bd8ecc7721771db0194
Ann (앤) <33626219+jhn3981@users.noreply.github.com>
2022-06-28 02:08:51 +0900

    feat(design-system-core): withVariation 함수를 구현한다 (#19)

--- packages/design-system-core/src/index.ts
+++ packages/design-system-core/src/index.ts
@@ -2,3 +2,5 @@ export { ThemeProvider, useTheme } from './lib/ThemeProvider';
 export { Box } from './lib/Box';
 export type { BoxProps } from './lib/Box';
 export type { ResponsiveValue } from './lib/types';
+export { propVariant } from './lib/propVariant';
+export { withVariation } from './lib/withVariation';
---
+++ packages/design-system-core/src/lib/withVariation/index.ts
@@ -0,0 +1,1 @@
+export { withVariation } from './withVariation';
