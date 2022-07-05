8047d7814341083d4d04f6720fbb32efa6fe91a7
Hanna Jeong <jhn3981@gmail.com>
2022-07-06 03:08:47 +0900

    fix(vibrant-components): Input 컴포넌트에 ref를 추가한다

--- packages/vibrant-components/src/lib/Input/Input.native.tsx
+++ packages/vibrant-components/src/lib/Input/Input.native.tsx
@@ -1,31 +1,38 @@
-import { useState } from 'react';
+import type { ForwardedRef } from 'react';
+import { forwardRef, useState } from 'react';
 import type { NativeSyntheticEvent, TextInputChangeEventData, TextInputKeyPressEventData } from 'react-native';
 import { TextInput } from 'react-native';
 import { Box } from '@vibrant-ui/core';
 import { withInputVariation } from './InputProps';
 
 export const Input = withInputVariation(
-  ({ placeholder, defaultValue, onFocus, onBlur, onKeyPress, onValueChange, replaceValue }) => {
-    const [value, setValue] = useState(defaultValue ?? '');
+  forwardRef(
+    (
+      { placeholder, defaultValue, onFocus, onBlur, onKeyPress, onValueChange, replaceValue },
+      ref: ForwardedRef<TextInput>
+    ) => {
+      const [value, setValue] = useState(defaultValue ?? '');
 
-    return (
-      <Box
-        base={TextInput}
-        placeholder={placeholder}
-        value={value}
-        onFocus={() => onFocus?.()}
-        onBlur={() => onBlur?.()}
-        onKeyPress={(event: NativeSyntheticEvent<TextInputKeyPressEventData>) => {
-          onKeyPress?.({ key: event.nativeEvent.key, prevent: event.preventDefault });
-        }}
-        onChange={(event: NativeSyntheticEvent<TextInputChangeEventData>) => {
-          const replacedValue = replaceValue(event.nativeEvent.text);
+      return (
+        <Box
+          ref={ref}
+          base={TextInput}
+          placeholder={placeholder}
+          value={value}
+          onFocus={() => onFocus?.()}
+          onBlur={() => onBlur?.()}
+          onKeyPress={(event: NativeSyntheticEvent<TextInputKeyPressEventData>) => {
+            onKeyPress?.({ key: event.nativeEvent.key, prevent: event.preventDefault });
+          }}
+          onChange={(event: NativeSyntheticEvent<TextInputChangeEventData>) => {
+            const replacedValue = replaceValue(event.nativeEvent.text);
 
-          setValue(replacedValue);
+            setValue(replacedValue);
 
-          onValueChange?.(replacedValue);
-        }}
-      />
-    );
-  }
+            onValueChange?.(replacedValue);
+          }}
+        />
+      );
+    }
+  )
 );
