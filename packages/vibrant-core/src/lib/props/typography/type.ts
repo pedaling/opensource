ae0f099db346b3c18f6ba982d981048868a659a3
peter <peter@101.inc>
2022-07-06 03:26:07 +0900

    fix: 타입수정

--- packages/vibrant-core/src/lib/props/typography/type.ts
+++ packages/vibrant-core/src/lib/props/typography/type.ts
@@ -4,9 +4,10 @@ import type { ResponsiveValue } from '../../types';
 export type TypographyProps = {
   typography?: ResponsiveValue<TypographyKind>;
   fontFamily?: ResponsiveValue<string>;
-  lineHeight?: ResponsiveValue<string | number>;
+  fontSize?: ResponsiveValue<string | number>;
   fontWeight?: ResponsiveValue<TypographyWeight>;
   fontStyle?: ResponsiveValue<'italic' | 'normal' | 'oblique'>;
+  lineHeight?: ResponsiveValue<string | number>;
   letterSpacing?: ResponsiveValue<string | number>;
   textAlign?: ResponsiveValue<'center' | 'end' | 'justify' | 'left' | 'match-parent' | 'right' | 'start'>;
   textTransform?: ResponsiveValue<'capitalize' | 'full-size-kana' | 'full-width' | 'lowercase' | 'none' | 'uppercase'>;
