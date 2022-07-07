fadf8b6738302cb8383b5d860528483ab75be6ac
peter <peter@101.inc>
2022-07-07 19:14:34 +0900

    test: test case 추가

--- packages/vibrant-components/src/lib/Stack/Stack.spec.tsx
+++ packages/vibrant-components/src/lib/Stack/Stack.spec.tsx
@@ -10,6 +10,22 @@ describe('<Stack />', () => {
 
   let element: HTMLElement;
 
+  describe('when empty stack rendered', () => {
+    beforeEach(async () => {
+      renderer = render(<Stack data-testid="stack" direction="vertical" />);
+
+      element = await renderer.findByTestId('stack');
+    });
+
+    it('div element created', () => {
+      expect(element.tagName).toBe('DIV');
+    });
+
+    it('match snapshot', () => {
+      expect(renderer.container).toMatchSnapshot();
+    });
+  });
+
   describe('when vertical stack with spacing rendered', () => {
     beforeEach(async () => {
       renderer = render(
@@ -22,7 +38,7 @@ describe('<Stack />', () => {
       element = await renderer.findByTestId('stack');
     });
 
-    it('flex-direction is row', async () => {
+    it('flex-direction is column', async () => {
       expect(element).toHaveStyleRule('flex-direction', 'column');
     });
 
@@ -51,7 +67,7 @@ describe('<Stack />', () => {
       element = await renderer.findByTestId('stack');
     });
 
-    it('flex-direction is column', async () => {
+    it('flex-direction is row', async () => {
       expect(element).toHaveStyleRule('flex-direction', 'row');
     });
 
@@ -104,6 +120,23 @@ describe('<Stack />', () => {
 
     it('match snapshot', async () => {
       expect(renderer.container).toMatchSnapshot();
+    });
+  });
+
+  describe('when children is falsy value', () => {
+    beforeEach(async () => {
+      renderer = render(
+        <Stack data-testid="stack" direction="vertical" spacing={20}>
+          {false}
+          {null}
+        </Stack>
+      );
+
+      element = await renderer.findByTestId('stack');
+    });
+
+    it('space not exists', () => {
+      expect(element.childElementCount).toBe(0);
     });
   });
 });
