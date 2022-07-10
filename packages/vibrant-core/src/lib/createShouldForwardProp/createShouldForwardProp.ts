7b1d6271a07d601ed3187845a44c22bc4a475536
Ann <jhn3981@gmail.com>
2022-07-10 15:40:58 +0900

    fix(vibrant-core): systemProp이 아닌 prop은 전부 forward 하도록 수정한다

--- packages/vibrant-core/src/lib/createShouldForwardProp/createShouldForwardProp.ts
+++ packages/vibrant-core/src/lib/createShouldForwardProp/createShouldForwardProp.ts
@@ -1,8 +1,7 @@
 import memoize from '@emotion/memoize';
-import { isPropValid } from '../isPropValid';
 
 export const createShouldForwardProp = (props: string[]) => {
   const regex = new RegExp(`^(${props.join('|')})$`);
 
-  return memoize((prop: string) => isPropValid(prop) && !regex.test(prop));
+  return memoize((prop: string) => !regex.test(prop));
 };
