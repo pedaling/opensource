47b5edf3693a953fedeecbaf4ad03f4e178babea
Ann (앤) <33626219+jhn3981@users.noreply.github.com>
2022-07-10 15:50:22 +0900

    fix(vibrant-core): systemProp이 아닌 prop은 전부 forward 하도록 수정한다 (#55)

--- packages/vibrant-core/src/lib/isPropValid/isPropValid.ts
+++
@@ -1,5 +0,0 @@
-import emotionPropValid from '@emotion/is-prop-valid';
-
-const validPropNames = ['as', 'base'];
-
-export const isPropValid = (propName: string) => emotionPropValid(propName) || validPropNames.includes(propName);
