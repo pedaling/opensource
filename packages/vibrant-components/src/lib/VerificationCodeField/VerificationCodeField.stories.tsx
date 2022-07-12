e71ff0bd951c3421c4429a8cfb898f3edaf688cf
Hanna Jeong <jhn3981@gmail.com>
2022-07-13 00:16:24 +0900

    fix: on으로 시작하는 prop이 자동으로 action이 되도록 수정한다

--- packages/vibrant-components/src/lib/VerificationCodeField/VerificationCodeField.stories.tsx
+++ packages/vibrant-components/src/lib/VerificationCodeField/VerificationCodeField.stories.tsx
@@ -9,11 +9,6 @@ export default {
     state: 'default',
     errorMessage: '숫자코드가 맞지 않습니다. 다시 입력해주세요.',
   },
-  argTypes: {
-    onValueChange: {
-      action: 'onValueChange',
-    },
-  },
 } as ComponentMeta<typeof VerificationCodeField>;
 
 export const Basic: ComponentStory<typeof VerificationCodeField> = props => <VerificationCodeField {...props} />;
