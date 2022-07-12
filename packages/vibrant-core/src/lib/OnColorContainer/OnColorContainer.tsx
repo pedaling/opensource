d91081b892932da706ebfc9a0ab21055d46cb5b9
Ann <jhn3981@gmail.com>
2022-07-12 17:47:33 +0900

    refactor(vibrant-core): OnColorContainer가 Theme onColor 값을 토큰으로 업데이트하게 수정한다

--- packages/vibrant-core/src/lib/OnColorContainer/OnColorContainer.tsx
+++ packages/vibrant-core/src/lib/OnColorContainer/OnColorContainer.tsx
@@ -1,64 +1,21 @@
-import type { FC, ReactElement, ReactNode } from 'react';
-import { cloneElement } from 'react';
-import { Global } from '@emotion/react';
+import type { FC, ReactElement } from 'react';
 import type { OnColorToken } from '@vibrant-ui/theme';
-import { Box } from '../Box';
-import { buildStyle } from '../buildStyle';
-import { useCurrentTheme } from '../ThemeProvider';
-import type { ResponsiveValue } from '../types';
+import { ThemeProvider } from '../ThemeProvider';
 
 type OnColorContainerProps = {
-  color: ResponsiveValue<OnColorToken>;
-} & (
-  | {
-      injectTo: 'root' | 'self';
-      children: ReactNode;
-    }
-  | {
-      injectTo?: 'forward';
-      children: ReactElement;
-    }
-);
-
-export const OnColorContainer: FC<OnColorContainerProps> = ({ color, injectTo = 'forward', children }) => {
-  const { theme } = useCurrentTheme({ root: injectTo === 'root' });
-  const { colors } = theme;
-
-  const responsiveColor = Array.isArray(color) ? color : [color];
-
-  const forwardStyle = buildStyle(
-    responsiveColor.map(value => ({
-      color: colors[value],
-      systemOnColor: colors[value],
-    })),
-    { theme }
-  );
-
-  const rootStyle = buildStyle(
-    responsiveColor.map(value => ({
-      ':root': {
-        '--system-on-color': colors[value],
-      },
-    })),
-    { theme }
-  );
-
-  if (injectTo === 'root') {
-    return (
-      <>
-        <Global styles={rootStyle} />
-        {children}
-      </>
-    );
-  }
-
-  if (injectTo === 'self') {
-    return (
-      <Box width="100%" height="100%">
-        {children}
-      </Box>
-    );
-  }
-
-  return <Box base={(props: Record<string, any>) => cloneElement(children as ReactElement, props)} {...forwardStyle} />;
+  color: OnColorToken;
+  children: ReactElement;
 };
+
+export const OnColorContainer: FC<OnColorContainerProps> = ({ color, children }) => (
+  <ThemeProvider
+    theme={{
+      colors: {
+        light: { onColor: `$colors.${color}` },
+        dark: { onColor: `$colors.${color}` },
+      },
+    }}
+  >
+    {children}
+  </ThemeProvider>
+);
