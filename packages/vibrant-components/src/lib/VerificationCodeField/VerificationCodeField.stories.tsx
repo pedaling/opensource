63481bd77b35f8a7df6b5453bd3f0e55271c2511
Hanna Jeong <jhn3981@gmail.com>
2022-07-13 00:08:47 +0900

    feat(vibrant-components): onValueChange를 추가한다

--- packages/vibrant-components/src/lib/VerificationCodeField/VerificationCodeField.stories.tsx
+++ packages/vibrant-components/src/lib/VerificationCodeField/VerificationCodeField.stories.tsx
@@ -9,6 +9,11 @@ export default {
     state: 'default',
     errorMessage: '숫자코드가 맞지 않습니다. 다시 입력해주세요.',
   },
+  argTypes: {
+    onValueChange: {
+      action: 'onValueChange',
+    },
+  },
 } as ComponentMeta<typeof VerificationCodeField>;
 
 export const Basic: ComponentStory<typeof VerificationCodeField> = props => <VerificationCodeField {...props} />;
