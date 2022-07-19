cc4075bf8e0b4877ad3369c8c93615f321cb92c1
Ann <jhn3981@gmail.com>
2022-07-19 15:32:22 +0900

    feat(vibrant-utils): range 함수를 추가한다

--- packages/vibrant-utils/src/index.ts
+++ packages/vibrant-utils/src/index.ts
@@ -2,5 +2,6 @@ export type { DeepPartial } from './lib/type/DeepPartial';
 export type { Path } from './lib/type/Path';
 export { isDefined } from './lib/isDefined';
 export { get } from './lib/get';
+export { range } from './lib/range';
 export { isRecord } from './lib/isRecord';
 export type { DeepWritable } from './lib/type/DeepWritable';
---
+++ packages/vibrant-utils/src/lib/range/range.ts
@@ -0,0 +1,1 @@
+export const range = (length: number): any[] => Array.from({ length }).map((_, index) => index);
