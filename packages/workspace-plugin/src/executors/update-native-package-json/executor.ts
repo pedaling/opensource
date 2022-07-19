26a0ffb0a53994da325a36fbc605136a833c67b7
peter <tbvjaos510@naver.com>
2022-07-20 00:41:38 +0900

    fix: git push가 잘되도록 한다 (#79)

--- packages/workspace-plugin/src/executors/update-native-package-json/executor.ts
+++ packages/workspace-plugin/src/executors/update-native-package-json/executor.ts
@@ -1,8 +1,9 @@
-import { execSync } from 'child_process';
 import * as fs from 'fs';
 import type { ExecutorContext } from 'nx/src/config/misc-interfaces';
 import { getProjectRoots } from 'nx/src/utils/command-line-utils';
 import { joinPathFragments } from 'nx/src/utils/path';
+import { commit, formatCommitMessage } from '@jscutlery/semver/src/executors/version/utils/commit';
+import { addToStage } from '@jscutlery/semver/src/executors/version/utils/git';
 import type { UpdateNativePackageJsonExecutorSchema } from './schema';
 
 export default async function runExecutor(options: UpdateNativePackageJsonExecutorSchema, context: ExecutorContext) {
@@ -18,9 +19,22 @@ export default async function runExecutor(options: UpdateNativePackageJsonExecut
 
   await fs.promises.writeFile(nativePackageJsonPath, JSON.stringify(nativePackageJson, null, 2).concat('\n'));
 
-  execSync(`git add ${nativePackageJsonPath}`);
+  await addToStage({
+    paths: [nativePackageJsonPath],
+    dryRun: false,
+  }).toPromise();
 
-  execSync('git commit --amend --no-edit');
+  await commit({
+    dryRun: false,
+    projectName: context.projectName,
+    commitMessage: formatCommitMessage({
+      // eslint-disable-next-line no-template-curly-in-string
+      commitMessageFormat: 'chore(${projectName}): release version ${version}',
+      projectName: `${context.projectName}-native`,
+      version: nativePackageJson.version,
+    }),
+    noVerify: false,
+  }).toPromise();
 
   return {
     success: true,
