04bcedd115f0708988d472b10ee765b20c1f71c2
Ann <jhn3981@gmail.com>
2022-07-26 15:07:30 +0900

    fix: update native jest config to use jest-expo preset

--- jest.preset.native.js
+++ jest.preset.native.js
@@ -1,6 +1,7 @@
+const expoPreset = require('jest-expo/jest-preset');
 const nxPreset = require('@nrwl/jest/preset').default;
 
 module.exports = {
   ...nxPreset,
-  setupFilesAfterEnv: ['jest-expo/src/preset/setup'],
+  ...expoPreset,
 };
--- packages/vibrant-motion/jest.config.native.ts
+++ packages/vibrant-motion/jest.config.native.ts
@@ -1,6 +1,3 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const reactNativePreset = require('react-native/jest-preset');
-
 export default {
   displayName: 'vibrant-motion-native',
   preset: '../../jest.preset.native.js',
@@ -10,5 +7,4 @@ export default {
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   coverageDirectory: '../../coverage/packages/vibrant-motion',
-  ...reactNativePreset,
 };
