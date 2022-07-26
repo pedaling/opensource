da4fb3e64bc754eff5d397e2a31793d611363ae1
peter <peter@101.inc>
2022-07-26 15:25:27 +0900

    fix: opacity에 token 추가

--- packages/vibrant-core/src/lib/props/display/type.ts
+++ packages/vibrant-core/src/lib/props/display/type.ts
@@ -1,9 +1,10 @@
+import type { OpacityToken } from '@vibrant-ui/theme';
 import type { ResponsiveValue } from '../../types';
 
 export type DisplayProps = {
   display?: ResponsiveValue<'flex' | 'inline-flex' | 'none'>;
   visibility?: ResponsiveValue<'collapse' | 'hidden' | 'visible'>;
-  opacity?: ResponsiveValue<number>;
+  opacity?: ResponsiveValue<OpacityToken | number>;
   hidden?: ResponsiveValue<boolean>;
   overflowX?: ResponsiveValue<'hidden' | 'scroll' | 'visible'>;
   overflowY?: ResponsiveValue<'hidden' | 'scroll' | 'visible'>;
