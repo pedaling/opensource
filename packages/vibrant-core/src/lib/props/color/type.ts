a87b04521cb1f2cc4e7447b24e7a5f6a94ebe41d
Ann (앤) <33626219+jhn3981@users.noreply.github.com>
2022-07-26 15:48:57 +0900

    feat(vibrant-core): add gradient systemProp (#111)

--- packages/vibrant-core/src/lib/props/color/type.ts
+++ packages/vibrant-core/src/lib/props/color/type.ts
@@ -1,7 +1,10 @@
+import type { GradientKind, LinearGradient } from '@vibrant-ui/theme';
 import type { ResponsiveValue } from '../../types';
 
 export type ColorProps = {
   color?: ResponsiveValue<string>;
   fill?: ResponsiveValue<string>;
   stroke?: ResponsiveValue<string>;
+  gradient?: ResponsiveValue<GradientKind>;
+  linearGradient?: ResponsiveValue<Omit<LinearGradient, 'type'>>;
 };
