4c6d00dc6266aab65fcef58b4f052ace8e575301
peter <peter@101.inc>
2022-07-27 13:40:42 +0900

    fix: add textOpacity

--- packages/vibrant-components/src/lib/Pressable/Pressable.tsx
+++ packages/vibrant-components/src/lib/Pressable/Pressable.tsx
@@ -1,7 +1,8 @@
 import type { KeyboardEventHandler } from 'react';
-import { useMemo, useState } from 'react';
+import { useState } from 'react';
 import { Box } from '@vibrant-ui/core';
 import { Transition } from '@vibrant-ui/motion';
+import { getOpacity } from './getOpacity';
 import { withPressableVariation } from './PressableProp';
 
 export const Pressable = withPressableVariation(
@@ -10,62 +11,62 @@ export const Pressable = withPressableVariation(
     const [isHovered, setIsHovered] = useState(false);
     const [isActivated, setIsActivated] = useState(false);
 
-    const currentOpacity = useMemo(() => {
-      if (disabled) {
-        return 0;
-      }
-
-      if (isActivated && interactions?.includes('active')) {
-        return 'overlay.active';
-      }
-
-      if (isFocused && interactions?.includes('focus')) {
-        return 'overlay.focus';
-      }
-
-      if (isHovered && overlayColor && interactions?.includes('hover')) {
-        return 'overlay.hover';
-      }
-
-      return 0;
-    }, [disabled, interactions, isActivated, isFocused, isHovered, overlayColor]);
+    const { overlayOpacity, textOpacity } = getOpacity({
+      interactions: interactions ?? [],
+      isActivated,
+      isFocused,
+      isHovered,
+      overlayColor,
+      disabled,
+    });
 
     return (
-      <Box
-        ref={innerRef}
-        as={as}
-        position="relative"
-        overflowX="hidden"
-        overflowY="hidden"
-        cursor={disabled ? 'default' : 'pointer'}
-        zIndex={0}
-        onClick={() => !disabled && onClick?.()}
-        onMouseEnter={() => setIsHovered(true)}
-        onMouseLeave={() => setIsHovered(false)}
-        onFocus={() => setIsFocused(true)}
-        onBlur={() => setIsFocused(false)}
-        onMouseDown={() => setIsActivated(true)}
-        onMouseUp={() => setIsActivated(false)}
-        {...(as === 'button'
-          ? {
-              disabled,
-              backgroundColor: 'inherit',
-              borderWidth: 0,
-              p: 0,
-            }
-          : {
-              tabIndex: disabled ? undefined : 0,
-              onKeyPress: (event => (!disabled && event.key === 'Enter' ? onClick?.() : null)) as KeyboardEventHandler,
-            })}
-        {...restProps}
-      >
-        {overlayColor && (
-          <Transition style={{ opacity: currentOpacity }} duration={200}>
-            <Box position="absolute" zIndex={-1} left={0} right={0} top={0} bottom={0} backgroundColor={overlayColor} />
-          </Transition>
-        )}
-        {children}
-      </Box>
+      <Transition style={{ opacity: textOpacity }} duration={200}>
+        <Box
+          ref={innerRef}
+          as={as}
+          position="relative"
+          overflowX="hidden"
+          overflowY="hidden"
+          cursor={disabled ? 'default' : 'pointer'}
+          zIndex={0}
+          onClick={() => !disabled && onClick?.()}
+          onMouseEnter={() => setIsHovered(true)}
+          onMouseLeave={() => setIsHovered(false)}
+          onFocus={() => setIsFocused(true)}
+          onBlur={() => setIsFocused(false)}
+          onMouseDown={() => setIsActivated(true)}
+          onMouseUp={() => setIsActivated(false)}
+          {...(as === 'button'
+            ? {
+                disabled,
+                backgroundColor: 'inherit',
+                borderWidth: 0,
+                p: 0,
+              }
+            : {
+                tabIndex: disabled ? undefined : 0,
+                onKeyPress: (event =>
+                  !disabled && event.key === 'Enter' ? onClick?.() : null) as KeyboardEventHandler,
+              })}
+          {...restProps}
+        >
+          {overlayColor && (
+            <Transition style={{ opacity: overlayOpacity }} duration={200}>
+              <Box
+                position="absolute"
+                zIndex={-1}
+                left={0}
+                right={0}
+                top={0}
+                bottom={0}
+                backgroundColor={overlayColor}
+              />
+            </Transition>
+          )}
+          {children}
+        </Box>
+      </Transition>
     );
   }
 );
---
+++ packages/vibrant-components/src/lib/Pressable/getOpacity.ts
@@ -0,0 +1,59 @@
+import type { ResponsiveValue } from '@vibrant-ui/core';
+import type { ColorToken } from '@vibrant-ui/theme';
+
+export const getOpacity = ({
+  disabled,
+  isFocused,
+  isActivated,
+  isHovered,
+  interactions,
+  overlayColor,
+}: {
+  disabled: boolean;
+  isFocused: boolean;
+  isHovered: boolean;
+  isActivated: boolean;
+  overlayColor?: ResponsiveValue<ColorToken>;
+  interactions: ('active' | 'focus' | 'hover')[];
+}) => {
+  const getOverlayOpacity = () => {
+    if (disabled || !overlayColor) {
+      return 0;
+    }
+
+    if (isActivated && interactions?.includes('active')) {
+      return 'overlay.active';
+    }
+
+    if (isFocused && interactions?.includes('focus')) {
+      return 'overlay.focus';
+    }
+
+    if (isHovered && overlayColor && interactions?.includes('hover')) {
+      return 'overlay.hover';
+    }
+
+    return 0;
+  };
+
+  const getTextOpacity = () => {
+    if (disabled || overlayColor) {
+      return 1;
+    }
+
+    if (isActivated && interactions?.includes('active')) {
+      return 'text.active';
+    }
+
+    if (isFocused && interactions?.includes('focus')) {
+      return 'text.focus';
+    }
+
+    return 1;
+  };
+
+  return {
+    overlayOpacity: getOverlayOpacity(),
+    textOpacity: getTextOpacity(),
+  };
+};
