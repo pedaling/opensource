79b5f691cc2707048b8d13fd0fcc4d5f48e99eb4
peter <tbvjaos510@naver.com>
2022-07-27 23:10:59 +0900

    feat(vibrant-components): add Switch component (#112)
    * fix: add textOpacity
    
    * feat(vibrant-components): add Switch component
    
    * fix: switch에 duration 추가
    
    * fix: fix conflict
    
    * fix: remove id, name
    
    * fix: color token을 수정한다
    
    * fix: update snapshot
    
    * fix: size 수정

---
+++ packages/vibrant-components/src/lib/Switch/Switch.tsx
@@ -0,0 +1,45 @@
+import { useEffect, useState } from 'react';
+import { Box } from '@vibrant-ui/core';
+import { Transition } from '@vibrant-ui/motion';
+import { Pressable } from '../Pressable';
+import { withSwitchVariation } from './SwitchProps';
+
+export const Switch = withSwitchVariation(
+  ({ defaultValue, opacity, roundRadius, roundSize, onValueChange, borderRadius, height, disabled, width }) => {
+    const [isChecked, setIsChecked] = useState(defaultValue);
+
+    useEffect(() => {
+      setIsChecked(defaultValue);
+    }, [defaultValue]);
+
+    const toggleChecked = () => {
+      setIsChecked(isCheckedState => !isCheckedState);
+
+      onValueChange?.(!isChecked);
+    };
+
+    return (
+      <Transition
+        animation={{
+          opacity,
+          backgroundColor: isChecked ? 'informative' : 'surface4',
+        }}
+        duration={200}
+      >
+        <Box
+          base={Pressable}
+          width={width}
+          height={height}
+          borderRadius={borderRadius}
+          p={3}
+          onClick={toggleChecked}
+          disabled={disabled}
+        >
+          <Transition animation={{ x: isChecked ? roundSize : 0 }} duration={200}>
+            <Box width={roundSize} height={roundSize} borderRadius={roundRadius} backgroundColor="white" />
+          </Transition>
+        </Box>
+      </Transition>
+    );
+  }
+);
