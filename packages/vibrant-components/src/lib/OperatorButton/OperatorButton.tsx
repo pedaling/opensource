5aba562d9aad1ab1120f7d54d77be7024434981b
Ann <jhn3981@gmail.com>
2022-08-02 20:14:06 +0900

    fix(vibrant-core): remove box 'as' prop default value

--- packages/vibrant-components/src/lib/OperatorButton/OperatorButton.tsx
+++ packages/vibrant-components/src/lib/OperatorButton/OperatorButton.tsx
@@ -3,14 +3,15 @@ import { Pressable } from '../Pressable';
 import { withOperationButtonVariation } from './OperatorButtonProps';
 
 export const OperatorButton = withOperationButtonVariation(
-  ({ IconComponent, backgroundColor, pressableProps, iconFill, ...restProps }) => (
+  ({ IconComponent, backgroundColor, iconFill, ...restProps }) => (
     <Box
       base={Pressable}
       p={7}
       borderWidth={0}
       borderRadius={2}
       backgroundColor={backgroundColor}
-      {...pressableProps}
+      overlayColor="onView1"
+      interactions={['hover', 'focus', 'active']}
       {...restProps}
     >
       <IconComponent size={16} fill={iconFill} />
