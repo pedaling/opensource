01dbee5782cff59e210d49457614aa8edcb037da
Ann <jhn3981@gmail.com>
2022-08-02 16:57:18 +0900

    fix(vibrant-core): remove ElementName default type

--- packages/vibrant-core/src/lib/Box/BoxProps.ts
+++ packages/vibrant-core/src/lib/Box/BoxProps.ts
@@ -2,20 +2,22 @@ import type { ComponentType, Ref } from 'react';
 import { createShouldForwardProp } from '../createShouldForwardProp';
 import type { SystemProps } from '../props';
 import { systemPropNames } from '../props';
+import type { ReactElementChild } from '../types';
 
 export type BoxProps<
-  BaseComponent extends ComponentType | unknown = unknown,
-  ElementName extends keyof JSX.IntrinsicElements = 'div',
-  ElementProps extends JSX.IntrinsicElements[ElementName] = JSX.IntrinsicElements[ElementName]
-> = Omit<
-  BaseComponent extends ComponentType<infer BaseComponentProps> ? BaseComponentProps : ElementProps,
-  keyof SystemProps
-> &
+  BaseComponent extends ComponentType | undefined = undefined,
+  ElementName extends keyof JSX.IntrinsicElements | undefined = undefined
+> = (BaseComponent extends ComponentType<infer BaseComponentProps>
+  ? BaseComponentProps
+  : ElementName extends keyof JSX.IntrinsicElements
+  ? Omit<JSX.IntrinsicElements[ElementName], keyof SystemProps>
+  : Record<never, never>) &
   SystemProps & {
     as?: ElementName;
     base?: BaseComponent;
     id?: string;
     ref?: Ref<BaseComponent extends abstract new (...args: any) => any ? InstanceType<BaseComponent> : HTMLElement>;
+    children?: ReactElementChild | ReactElementChild[];
   };
 
 export const shouldForwardProp = createShouldForwardProp(systemPropNames);
