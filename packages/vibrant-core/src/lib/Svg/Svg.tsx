fb6bdd99414c4fba18be0d749fbf89c289884ecd
Ann (앤) <33626219+jhn3981@users.noreply.github.com>
2022-08-03 17:12:36 +0900

    fix(vibrant-core): fix Svg component type error (#146)

--- packages/vibrant-core/src/lib/Svg/Svg.tsx
+++ packages/vibrant-core/src/lib/Svg/Svg.tsx
@@ -1,44 +1,53 @@
 /* eslint-disable @typescript-eslint/naming-convention */
 import type { FC } from 'react';
 import { Box } from '../Box';
-import type { BaseSvgProps, SvgProps } from './SvgProps';
+import type {
+  ClipPathProps,
+  DefsProps,
+  GProps,
+  LinearGradientProps,
+  MaskProps,
+  PathProps,
+  StopProps,
+  SvgProps,
+} from './SvgProps';
 
-const Path: FC<BaseSvgProps> = props => <Box as="path" {...props} />;
+const ClipPath: FC<ClipPathProps> = props => <Box as="clipPath" {...props} />;
 
-const Mask: FC<BaseSvgProps> = props => <Box as="mask" {...props} />;
+const Defs: FC<DefsProps> = props => <Box as="defs" {...props} />;
 
-const G: FC<BaseSvgProps> = props => <Box as="g" {...props} />;
+const G: FC<GProps> = props => <Box as="g" {...props} />;
 
-const Defs: FC<BaseSvgProps> = props => <Box as="defs" {...props} />;
+const LinearGradient: FC<LinearGradientProps> = props => <Box as="linearGradient" {...props} />;
 
-const ClipPath: FC<BaseSvgProps> = props => <Box as="clipPath" {...props} />;
+const Mask: FC<MaskProps> = props => <Box as="mask" {...props} />;
 
-const LinearGradient: FC<BaseSvgProps> = props => <Box as="linearGradient" {...props} />;
+const Path: FC<PathProps> = props => <Box as="path" {...props} />;
 
-const Stop: FC<BaseSvgProps> = props => <Box as="stop" {...props} />;
+const Stop: FC<StopProps> = props => <Box as="stop" {...props} />;
 
 export const Svg: FC<SvgProps> & {
-  G: FC<BaseSvgProps>;
-  Mask: FC<BaseSvgProps>;
-  Path: FC<BaseSvgProps>;
-  Defs: FC<BaseSvgProps>;
-  ClipPath: FC<BaseSvgProps>;
-  LinearGradient: FC<BaseSvgProps>;
-  Stop: FC<BaseSvgProps>;
+  ClipPath: FC<ClipPathProps>;
+  Defs: FC<DefsProps>;
+  G: FC<GProps>;
+  LinearGradient: FC<LinearGradientProps>;
+  Mask: FC<MaskProps>;
+  Path: FC<PathProps>;
+  Stop: FC<StopProps>;
 } = ({ width, height, ...restProps }) => (
-  <Box as="svg" display="inline-flex" width={width || 'auto'} height={height || 'auto'} {...restProps} />
+  <Box as="svg" display="inline-flex" width={width || 'auto'} height={height || 'auto'} {...restProps}></Box>
 );
-
-Svg.Mask = Mask;
-
-Svg.G = G;
-
-Svg.Path = Path;
-
-Svg.Defs = Defs;
 
 Svg.ClipPath = ClipPath;
 
+Svg.Defs = Defs;
+
+Svg.G = G;
+
 Svg.LinearGradient = LinearGradient;
+
+Svg.Mask = Mask;
+
+Svg.Path = Path;
 
 Svg.Stop = Stop;
