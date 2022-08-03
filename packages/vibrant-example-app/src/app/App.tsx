9de3d99755eb29fbf247bfba4470cf45b1049ae5
YimJiYoung <chiy323@gmail.com>
2022-08-03 17:57:21 +0900

    feat(vibrant-example-app): add nativeShadow dependency to VibrantProvider

--- packages/vibrant-example-app/src/app/App.tsx
+++ packages/vibrant-example-app/src/app/App.tsx
@@ -1,10 +1,13 @@
 import { useFonts } from 'expo-font';
 import { LinearGradient } from 'expo-linear-gradient';
+import { Shadow } from 'react-native-shadow-2';
 import { Title, VStack } from '@vibrant-ui/components';
 import { VibrantProvider } from '@vibrant-ui/core';
+import { createShadowsComponent } from '@vibrant-ui/utils';
 
 const dependencies = {
   nativeLinearGradient: LinearGradient,
+  nativeShadow: createShadowsComponent(Shadow),
 };
 
 const App = () => {
