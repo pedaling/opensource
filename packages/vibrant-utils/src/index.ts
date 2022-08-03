2a05236055fb1f04031a4a073f171f38a8bb01dd
Ann <jhn3981@gmail.com>
2022-08-03 21:56:21 +0900

    feat(vibrant-utils): add DistributiveOmit utility type

--- packages/vibrant-utils/src/index.ts
+++ packages/vibrant-utils/src/index.ts
@@ -1,10 +1,11 @@
 export type { DeepPartial } from './lib/type/DeepPartial';
+export type { DeepWritable } from './lib/type/DeepWritable';
+export type { DistributiveOmit } from './lib/type/DistributiveOmit';
 export type { Path } from './lib/type/Path';
 export { isDefined } from './lib/isDefined';
 export { get } from './lib/get';
 export { range } from './lib/range';
 export { isRecord } from './lib/isRecord';
-export type { DeepWritable } from './lib/type/DeepWritable';
 export { uuidV4 } from './lib/uuidV4';
 export { useSafeDeps } from './lib/useSafeDeps';
 export { createShadowsComponent } from './lib/createShadowsComponent';
