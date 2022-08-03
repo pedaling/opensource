9eed9da7a48255c1e906ae37caae2a7f8bfb09e2
Ann <jhn3981@gmail.com>
2022-08-03 17:09:26 +0900

    fix(vibrant-core): fix Svg component type error

--- packages/vibrant-core/src/lib/Svg/SvgProps.ts
+++ packages/vibrant-core/src/lib/Svg/SvgProps.ts
@@ -1,10 +1,21 @@
-import type { SVGProps } from 'react';
 import type { BoxProps } from '../Box';
 
-export type BaseSvgProps = Omit<
-  SVGProps<SVGElement>,
-  keyof BoxProps<undefined, 'clipPath' | 'defs' | 'g' | 'linearGradient' | 'mark' | 'path' | 'stop' | 'svg'>
-> & { fill?: string; id?: string };
+export type ClipPathProps = BoxProps<undefined, 'clipPath'>;
 
-export type SvgProps = Omit<BaseSvgProps, 'fill'> &
-  Pick<BoxProps, 'children' | 'fill' | 'height' | 'id' | 'opacity' | 'width'>;
+export type DefsProps = BoxProps<undefined, 'defs'>;
+
+// eslint-disable-next-line @typescript-eslint/naming-convention
+export type GProps = BoxProps<undefined, 'g'>;
+
+export type LinearGradientProps = BoxProps<undefined, 'linearGradient'>;
+
+export type MaskProps = BoxProps<undefined, 'mask'>;
+
+export type PathProps = BoxProps<undefined, 'path'>;
+
+export type StopProps = BoxProps<undefined, 'stop'>;
+
+export type SvgProps = Pick<
+  BoxProps<undefined, 'svg'>,
+  keyof JSX.IntrinsicElements['svg'] | 'children' | 'fill' | 'height' | 'id' | 'width'
+>;
