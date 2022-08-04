1115940d51789b19c9d5b8ca4b230d8ce930fb73
peter <peter@101.inc>
2022-08-04 13:31:40 +0900

    fix: 50%대신 10000px로 적용되도록 하낟

--- packages/vibrant-theme/src/lib/theme/baseTheme.ts
+++ packages/vibrant-theme/src/lib/theme/baseTheme.ts
@@ -160,7 +160,7 @@ export const baseTheme: Theme = {
     2: 12,
     3: 16,
     4: 20,
-    5: '50%',
+    5: 10000,
   },
   mode: 'light',
 };
--- packages/vibrant-theme/src/types/BorderRadius.ts
+++ packages/vibrant-theme/src/types/BorderRadius.ts
@@ -1,5 +1,5 @@
 export type BorderRadiusLevel = 0 | 1 | 2 | 3 | 4 | 5;
 
 export type ThemeBorderRadius = {
-  [key in BorderRadiusLevel]: number | '50%';
+  [key in BorderRadiusLevel]: number;
 };
