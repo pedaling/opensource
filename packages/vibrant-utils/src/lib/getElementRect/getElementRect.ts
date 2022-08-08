70fb721b90e7195aa2dc4ba307439a30449626c2
Mia(임지영) <37496919+YimJiYoung@users.noreply.github.com>
2022-08-08 14:35:00 +0900

    feat(vibrant-utils): getElementRect 유틸 함수를 추가한다. (#160)
    * feat: element의 위치, 크기를 가져오는 getElementRect 유틸 함수를 추가한다
    
    * feat(vibrant-utils): 윈도우 사이즈를 가져오는 getWindowSize 유틸 함수를 추가한다
    
    * refactor: getElementRect native 파일 삭제
    
    * refactor: getWindowSize 유틸 함수 삭제
    
    * chore: lint

---
+++ packages/vibrant-utils/src/lib/getElementRect/getElementRect.ts
@@ -0,0 +1,22 @@
+import type { Rect, TargetElement } from './type';
+
+export function getElementRect(element: TargetElement): Promise<Rect> {
+  return new Promise(resolve => {
+    if (element.measureInWindow) {
+      element.measureInWindow((x: number, y: number, width: number, height: number) => {
+        resolve({ x, y, width, height });
+      });
+    } else {
+      const observer = new IntersectionObserver(([entry], observer) => {
+        observer.disconnect();
+
+        const { boundingClientRect } = entry;
+        const { x, y, width, height } = boundingClientRect;
+
+        resolve({ x, y, width, height });
+      });
+
+      observer.observe(element);
+    }
+  });
+}
