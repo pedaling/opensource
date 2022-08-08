c7aa4c0ec671273eb3e1de312b5fa1d5a2714ad1
YimJiYoung <chiy323@gmail.com>
2022-08-08 11:14:57 +0900

    refactor: getElementRect native 파일 삭제

--- packages/vibrant-utils/src/lib/getElementRect/getElementRect.native.ts
+++
@@ -1,9 +0,0 @@
-import type { Rect, TargetElement } from './type';
-
-export function getElementRect(element: TargetElement): Promise<Rect> {
-  return new Promise(resolve => {
-    element.measureInWindow((x: number, y: number, width: number, height: number) => {
-      resolve({ x, y, width, height });
-    });
-  });
-}
