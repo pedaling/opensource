4a14ea47c604d8b8f70f2c7f34841595591b52d9
peter <peter@101.inc>
2022-08-11 14:22:53 +0900

    fix: optional value

--- packages/vibrant-components/src/lib/DatePickerField/DatePickerFieldProps.ts
+++ packages/vibrant-components/src/lib/DatePickerField/DatePickerFieldProps.ts
@@ -2,10 +2,17 @@ import { withVariation } from '@vibrant-ui/core';
 import type { BaseInputProps } from '../../types';
 
 export type DatePickerFieldProps = BaseInputProps<Date> & {
-  placeholder?: string;
-  label?: string;
   helperText?: string;
   state?: 'default' | 'error';
-};
+} & (
+    | {
+        label?: never;
+        placeholder?: string;
+      }
+    | {
+        label?: string;
+        placeholder?: never;
+      }
+  );
 
 export const withDatePickerFieldVariation = withVariation<DatePickerFieldProps>('DatePickerField')();
--- packages/vibrant-components/src/lib/RangePickerField/RangePickerFieldProps.ts
+++ packages/vibrant-components/src/lib/RangePickerField/RangePickerFieldProps.ts
@@ -2,10 +2,17 @@ import { withVariation } from '@vibrant-ui/core';
 import type { BaseInputProps } from '../../types';
 
 export type RangePickerFieldProps = BaseInputProps<{ start: Date; end: Date }> & {
-  placeholder?: string;
-  label?: string;
   helperText?: string;
   state?: 'default' | 'error';
-};
+} & (
+    | {
+        label?: never;
+        placeholder?: string;
+      }
+    | {
+        label?: string;
+        placeholder?: never;
+      }
+  );
 
 export const withRangePickerFieldVariation = withVariation<RangePickerFieldProps>('RangePickerField')();
