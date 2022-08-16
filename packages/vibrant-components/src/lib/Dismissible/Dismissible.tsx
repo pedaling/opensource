c76090061fc7a2441a043977dda54c62cd7efa1e
YimJiYoung <chiy323@gmail.com>
2022-08-16 13:58:18 +0900

    fix(vibrant-components): don't overried children ref in Dismissible

--- packages/vibrant-components/src/lib/Dismissible/Dismissible.tsx
+++ packages/vibrant-components/src/lib/Dismissible/Dismissible.tsx
@@ -1,5 +1,5 @@
 import type { FC } from 'react';
-import { cloneElement, useEffect, useRef } from 'react';
+import { cloneElement, useCallback, useEffect, useMemo, useRef } from 'react';
 import { useGlobalEvent } from '@vibrant-ui/core';
 import type { ClickEvent } from '@vibrant-ui/core';
 import { getElementRect } from '@vibrant-ui/utils';
@@ -8,7 +8,7 @@ import type { DismissibleProps } from './DismissibleProps';
 export const Dismissible: FC<DismissibleProps> = ({ active = false, onDismiss, children }) => {
   const { addEventListener, removeEventListener } = useGlobalEvent();
 
-  const targetRef = useRef<HTMLElement>(null);
+  const targetRef = useRef<HTMLElement | null>(null);
 
   useEffect(() => {
     if (!active) {
@@ -43,5 +43,22 @@ export const Dismissible: FC<DismissibleProps> = ({ active = false, onDismiss, c
     };
   }, [active, addEventListener, onDismiss, removeEventListener]);
 
-  return <>{cloneElement(children, { ref: targetRef })}</>;
+  const forwardTargetRef = useCallback(
+    (node: HTMLElement | null) => {
+      if (children.ref) {
+        if (typeof children.ref === 'function') {
+          children.ref(node);
+        } else {
+          children.ref.current = node;
+        }
+      }
+
+      targetRef.current = node;
+    },
+    [children]
+  );
+
+  const clonedChildren = useMemo(() => cloneElement(children, { ref: forwardTargetRef }), [children, forwardTargetRef]);
+
+  return <>{clonedChildren}</>;
 };
