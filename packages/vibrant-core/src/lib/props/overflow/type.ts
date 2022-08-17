d67e21fbe12d2d03eeabc0bc696fa804fe5dd0ec
YimJiYoung <chiy323@gmail.com>
2022-08-17 14:49:02 +0900

    chore(vibrant-core): overflow type 추가

--- packages/vibrant-core/src/lib/props/overflow/type.ts
+++ packages/vibrant-core/src/lib/props/overflow/type.ts
@@ -1,6 +1,7 @@
 import type { ResponsiveValue } from '../../../types';
 
 export type OverflowSystemProps = {
+  overflow?: ResponsiveValue<'hidden' | 'scroll' | 'visible'>;
   overflowX?: ResponsiveValue<'hidden' | 'scroll' | 'visible'>;
   overflowY?: ResponsiveValue<'hidden' | 'scroll' | 'visible'>;
   hideScroll?: ResponsiveValue<boolean>;
