65841b1aed4d60f2e1384e95ad4507067a017d66
YimJiYoung <chiy323@gmail.com>
2022-08-18 22:24:16 +0900

    feat(vibrant-core): useInterpolation에서 추가적인 system prop을 입력받을 수 있게 한다

--- packages/vibrant-core/src/lib/useInterpolation/useInterpolation.ts
+++ packages/vibrant-core/src/lib/useInterpolation/useInterpolation.ts
@@ -1,12 +1,12 @@
-import { useMemo } from 'react';
 import { createInterpolation } from '../createInterpolation';
+import type { SystemProp } from '../createSystemProp';
 import { allSystemProps } from '../props';
 import { useCurrentTheme } from '../ThemeProvider';
 
-export const useInterpolation = () => {
+export const useInterpolation = (additionalSystemProps?: SystemProp[]) => {
   const { theme } = useCurrentTheme();
 
-  const interpolation = useMemo(() => createInterpolation(allSystemProps)(theme), [theme]);
+  const interpolation = createInterpolation([...allSystemProps, ...(additionalSystemProps ?? [])])(theme);
 
   return {
     interpolation,
