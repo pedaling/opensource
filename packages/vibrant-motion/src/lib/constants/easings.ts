4d65ff2cb6ecbd42ecfd6195ddaf5c374b0c5348
YimJiYoung <chiy323@gmail.com>
2022-08-22 14:57:45 +0900

    feat: easeIn/OutSine -> Quad로 수정

--- packages/vibrant-motion/src/lib/constants/easings.ts
+++ packages/vibrant-motion/src/lib/constants/easings.ts
@@ -1,11 +1,11 @@
 export type EasingDictionary = {
   linear: (t: number) => number;
-  easeInSine: (t: number) => number;
-  easeOutSine: (t: number) => number;
+  easeInQuad: (t: number) => number;
+  easeOutQuad: (t: number) => number;
 };
 
 export const easings: EasingDictionary = {
   linear: x => x,
-  easeInSine: x => 1 - Math.cos((x * Math.PI) / 2),
-  easeOutSine: x => Math.sin((x * Math.PI) / 2),
+  easeInQuad: x => x * x,
+  easeOutQuad: x => 1 - (1 - x) * (1 - x),
 };
