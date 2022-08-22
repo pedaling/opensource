5bb308598f3ed62fbf53e0d23bd5d9dc1a85d757
peter <peter@101.inc>
2022-08-22 13:25:18 +0900

    fix: withTransformStyle 네이티브 파일 분기를 제거한다

--- packages/vibrant-motion/src/lib/withTransformStyle/withTransformStyle.ts
+++ packages/vibrant-motion/src/lib/withTransformStyle/withTransformStyle.ts
@@ -1,1 +1,21 @@
-export const withTransformStyle = (style: any) => style;
+export const withTransformStyle = (style: any) => {
+  if (typeof window === 'undefined') {
+    const transform = [];
+    const { x, y, ...restStyle } = style;
+
+    if (x !== undefined) {
+      transform.push({ translateX: x });
+    }
+
+    if (y !== undefined) {
+      transform.push({ translateY: y });
+    }
+
+    return {
+      ...restStyle,
+      transform,
+    };
+  }
+
+  return style;
+};
