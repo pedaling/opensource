177b8ef31cb75a1221222ec2cfaf865ebdb61530
Ann (앤) <33626219+jhn3981@users.noreply.github.com>
2022-08-22 14:15:36 +0900

    feat(vibrant-website): vibrant website 랜딩페이지를 구현한다 (#205)

--- packages/vibrant-website/babel.config.js
+++ packages/vibrant-website/babel.config.js
@@ -1,3 +1,22 @@
+const path = require('path');
+
 module.exports = {
-  presets: [require.resolve('@docusaurus/core/lib/babel/preset')],
+  presets: [
+    require.resolve('@docusaurus/core/lib/babel/preset'),
+    [
+      '@babel/preset-react',
+      {
+        runtime: 'automatic',
+      },
+    ],
+  ],
+  plugins: [
+    [
+      'babel-plugin-tsconfig-paths',
+      {
+        root: path.join(__dirname, '../../'),
+        tsconfig: require.resolve('../../tsconfig.base.json'),
+      },
+    ],
+  ],
 };
--- packages/vibrant-website/src/pages/index.tsx
+++ packages/vibrant-website/src/pages/index.tsx
@@ -1,22 +1,7 @@
-import clsx from 'clsx';
 import React from 'react';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
-import HomepageFeatures from '../components/HomepageFeatures';
-import styles from './index.module.css';
-
-const HomepageHeader = () => {
-  const { siteConfig } = useDocusaurusContext();
-
-  return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-      </div>
-    </header>
-  );
-};
+import { Body, Display, Divider, HStack, Paper, Title, VStack } from '@vibrant-ui/components';
 
 const Home = (): JSX.Element => {
   const { siteConfig } = useDocusaurusContext();
@@ -26,10 +11,62 @@ const Home = (): JSX.Element => {
       title={`Hello from ${siteConfig.title}`}
       description="Open source design system for building modern, cross-platform web and mobile applications."
     >
-      <HomepageHeader />
-      <main>
-        <HomepageFeatures />
-      </main>
+      <VStack maxWidth={1024} width="100%" mx="auto" px={40}>
+        <HStack py={48}>
+          <VStack width="50%" justifyContent="center" spacing={10}>
+            <Display as="h1" level={4}>
+              Vibrant Design System
+            </Display>
+            <Body as="p" level={1}>
+              Class101의 사용자들이 보다 일관적인 서비스를 경험하기 위하여 효율적이고 우수한 성능의 프로덕트를 제작할 수
+              있도록 고안된 시스템입니다.
+            </Body>
+          </VStack>
+          <Paper width="50%" backgroundColor="surface1">
+            <img src="/img/hero.png" alt="" />
+          </Paper>
+        </HStack>
+
+        <Divider direction="horizontal" margin="none" />
+
+        <VStack py={64} spacing={24}>
+          <Title as="h2" level={4}>
+            Core Value
+          </Title>
+
+          <HStack spacing={64}>
+            <VStack spacing={12}>
+              <Paper width={120} height={120} backgroundColor="surface1" />
+              <Title as="h3" level={6}>
+                Performant
+              </Title>
+              <Body as="p" level={1} color="onView2">
+                효과적으로 제작된 Class101의 프로덕트는 최고의 성능으로 기능하여 유저에게 긍정적인 사용자 경험을 줄 수
+                있습니다.
+              </Body>
+            </VStack>
+            <VStack spacing={12}>
+              <Paper width={120} height={120} backgroundColor="surface1" />
+              <Title as="h3" level={6}>
+                Productive
+              </Title>
+              <Body as="p" level={1} color="onView2">
+                Class101의 빌더들이 보다 생산적으로 일할 수 있도록 하여 보다 적은 노력으로 같은 조건의 결과물을 산출할
+                수 있습니다.
+              </Body>
+            </VStack>
+            <VStack spacing={12}>
+              <Paper width={120} height={120} backgroundColor="surface1" />
+              <Title as="h3" level={6}>
+                Consistent
+              </Title>
+              <Body as="p" level={1} color="onView2">
+                사용자들이 플랫폼에 관계 없이 일관된 프로덕트를 경험할 수 있는 일관적인 사용자 경험을 줄 수 있습니다.
+              </Body>
+            </VStack>
+          </HStack>
+        </VStack>
+      </VStack>
     </Layout>
   );
 };
