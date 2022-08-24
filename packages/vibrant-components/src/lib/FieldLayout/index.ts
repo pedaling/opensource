f665dc75468e38413588bfc39bdb3c2d65531992
Ann (앤) <33626219+jhn3981@users.noreply.github.com>
2022-08-24 15:13:16 +0900

    feat(vibrant-components): add FieldLayout component (#223)

---
+++ packages/vibrant-components/src/lib/FieldLayout/index.ts
@@ -0,0 +1,1 @@
+export { FieldLayout } from './FieldLayout';
--- packages/vibrant-example-app/src/stories.ts
+++ packages/vibrant-example-app/src/stories.ts
@@ -9,6 +9,7 @@ export const stories = [
   require('../../vibrant-components/src/lib/Display/Display.stories.tsx'),
   require('../../vibrant-components/src/lib/Divider/Divider.stories.tsx'),
   require('../../vibrant-components/src/lib/Dropdown/Dropdown.stories.tsx'),
+  require('../../vibrant-components/src/lib/FieldLayout/FieldLayout.stories.tsx'),
   require('../../vibrant-components/src/lib/HStack/HStack.stories.tsx'),
   require('../../vibrant-components/src/lib/NotificationBadge/NotificationBadge.stories.tsx'),
   require('../../vibrant-components/src/lib/NumericField/NumericField.stories.tsx'),
