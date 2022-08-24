d7875566433216c25a301b269f8dcbe0495c7c1a
YimJiYoung <chiy323@gmail.com>
2022-08-24 14:41:22 +0900

    chore: storybook decorator provider에서 메모된 theme을 넘긴다

--- .storybook/decorators/withVibrantProvider.tsx
+++ .storybook/decorators/withVibrantProvider.tsx
@@ -2,28 +2,30 @@ import * as ReactSpring from '@react-spring/web';
 import type { DecoratorFn } from '@storybook/react';
 import { VibrantProvider } from '@vibrant-ui/core';
 
+const theme = {
+  typographyWeight: {
+    regular: {
+      fontFamily: 'Pretendard',
+      fontWeight: '400',
+    },
+    medium: {
+      fontFamily: 'Pretendard',
+      fontWeight: '500',
+    },
+    bold: {
+      fontFamily: 'Pretendard',
+      fontWeight: '700',
+    },
+    extraBold: {
+      fontFamily: 'Pretendard',
+      fontWeight: '800',
+    },
+  },
+};
+
 export const withVibrantProvider: DecoratorFn = storyFn => (
   <VibrantProvider
-    theme={{
-      typographyWeight: {
-        regular: {
-          fontFamily: 'Pretendard',
-          fontWeight: '400',
-        },
-        medium: {
-          fontFamily: 'Pretendard',
-          fontWeight: '500',
-        },
-        bold: {
-          fontFamily: 'Pretendard',
-          fontWeight: '700',
-        },
-        extraBold: {
-          fontFamily: 'Pretendard',
-          fontWeight: '800',
-        },
-      },
-    }}
+    theme={theme}
     dependencies={{
       reactSpringModule: ReactSpring,
     }}
