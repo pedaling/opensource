c3b28a42002c4120276f469f2010492b7717cd58
Hanna Jeong <jhn3981@gmail.com>
2022-08-24 14:08:48 +0900

    fix(vibrant-core): add ref to Text component

--- packages/vibrant-core/src/lib/Text/Text.tsx
+++ packages/vibrant-core/src/lib/Text/Text.tsx
@@ -1,11 +1,19 @@
 import type { FC } from 'react';
-import { createElement } from 'react';
+import { createElement, forwardRef } from 'react';
 import styled from '@emotion/styled';
 import type { TextProps } from './TextProps';
 import { interpolation, shouldForwardProp } from './TextProps';
 
-export const Text: FC<TextProps> = styled(({ as = 'span', ...restProps }) => createElement(as, restProps), {
-  shouldForwardProp,
-})(interpolation);
+export const Text: FC<TextProps> = styled(
+  forwardRef<unknown, TextProps>(({ as = 'span', ...restProps }, ref) =>
+    createElement(as, {
+      ref,
+      ...restProps,
+    })
+  ),
+  {
+    shouldForwardProp,
+  }
+)(interpolation);
 
 (Text as FC).displayName = 'Text';
