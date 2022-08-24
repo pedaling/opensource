5b66f18957abba975813ed863e183795696485a4
Ann (앤) <33626219+jhn3981@users.noreply.github.com>
2022-08-24 14:18:41 +0900

    fix(vibrant-core): add ref to Text component (#220)

--- packages/vibrant-core/src/lib/Text/Text.native.tsx
+++ packages/vibrant-core/src/lib/Text/Text.native.tsx
@@ -1,15 +1,16 @@
 import type { FC } from 'react';
+import { forwardRef } from 'react';
 import { Text as RNText, StyleSheet } from 'react-native';
 import styled from '@emotion/native';
 import type { TextProps } from './TextProps';
 import { interpolation, shouldForwardProp } from './TextProps';
 
 export const Text: FC<TextProps> = styled(
-  ({ style, ...restProps }: { style?: any }) => {
+  forwardRef<unknown, TextProps & { style?: any }>(({ style, ...restProps }, ref) => {
     const { props, ...restStyle } = StyleSheet.flatten(style);
 
-    return <RNText style={restStyle} {...restProps} {...props} />;
-  },
+    return <RNText ref={ref} style={restStyle} {...restProps} {...props} />;
+  }),
   {
     shouldForwardProp,
   }
