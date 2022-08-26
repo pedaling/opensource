7d17eedcc09065912a98feff3226652e4dc3a418
YimJiYoung <chiy323@gmail.com>
2022-08-26 14:04:26 +0900

    feat: native hide scroll 대응

--- packages/vibrant-core/src/lib/ScrollBox/props/scroll.native.ts
+++ packages/vibrant-core/src/lib/ScrollBox/props/scroll.native.ts
@@ -1,1 +1,13 @@
-export const scrollSystemProps = [];
+import { createSystemProp } from '../../createSystemProp';
+
+const hideScrollProp = createSystemProp({
+  property: 'hideScroll',
+  transform: () => ({
+    props: {
+      showsHorizontalScrollIndicator: false,
+      showsVerticalScrollIndicator: false,
+    },
+  }),
+});
+
+export const scrollSystemProps = [hideScrollProp];
