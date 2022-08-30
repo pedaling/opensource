aacc551241adc13535368864e03b27021a46145b
Hanna Jeong <jhn3981@gmail.com>
2022-08-30 18:44:39 +0900

    fix(vibrant-theme): update overlay opacity value

--- packages/vibrant-theme/src/lib/theme/lightModeOpacity.ts
+++ packages/vibrant-theme/src/lib/theme/lightModeOpacity.ts
@@ -2,9 +2,9 @@ import type { Opacity } from '../../types';
 
 export const lightModeOpacity: Opacity = {
   overlay: {
-    hover: 0.08,
-    focus: 0.12,
-    active: 0.16,
+    hover: 0.025,
+    focus: 0.075,
+    active: 0.075,
   },
   text: {
     focus: 0.8,
