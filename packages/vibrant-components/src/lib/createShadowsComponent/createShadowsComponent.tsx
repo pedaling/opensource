3bf2a71052a392c77ba8a4a91ba4cf1cfb07e309
Mia(임지영) <37496919+YimJiYoung@users.noreply.github.com>
2022-08-31 13:04:44 +0900

    feat: createSHadowsComponent에서 offset이 0인 경우 paintInside 옵션을 false로 준다 (#255)

--- packages/vibrant-components/src/lib/createShadowsComponent/createShadowsComponent.tsx
+++ packages/vibrant-components/src/lib/createShadowsComponent/createShadowsComponent.tsx
@@ -7,6 +7,7 @@ type ShadowComponentType = ComponentType<{
   distance?: any;
   offset?: any;
   viewStyle?: any;
+  paintInside?: boolean;
   children?: ReactNode;
 }>;
 
@@ -22,7 +23,11 @@ type ShadowsProps = {
 
 export const createShadowsComponent = (ShadowComponent: ShadowComponentType) => {
   const ShadowsComponent = forwardRef<any, ShadowsProps>(({ shadows, style, children }, ref) => (
-    <ShadowComponent viewStyle={shadows.length <= 1 ? style : undefined} {...(shadows[0] ?? { distance: 0 })}>
+    <ShadowComponent
+      paintInside={!(shadows?.[0].offset?.[0] === 0 && shadows?.[0].offset?.[1] === 0)}
+      viewStyle={shadows.length <= 1 ? style : undefined}
+      {...(shadows[0] ?? { distance: 0 })}
+    >
       {shadows.length <= 1 ? (
         <Box ref={ref}>{children}</Box>
       ) : (
