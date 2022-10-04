a367a25f6c6ada69f1cc59f1fb8581d3530d5f4e
lou-lee <lou@101.inc>
2022-10-04 19:01:49 +0900

    refactor: move HStack position into inside of provider

--- packages/vibrant-components/src/lib/ToastProvider/ToastProvider.tsx
+++ packages/vibrant-components/src/lib/ToastProvider/ToastProvider.tsx
@@ -1,6 +1,6 @@
 import { createContext, useContext, useEffect, useState } from 'react';
 import type { Dispatch, FC } from 'react';
-import { StackedPortal, Toast } from '@vibrant-ui/components';
+import { HStack, StackedPortal, Toast } from '@vibrant-ui/components';
 import type { ReactElementChild } from '@vibrant-ui/core';
 import { Transition } from '@vibrant-ui/motion';
 import type { ToastProps } from '../Toast/ToastProps';
@@ -39,7 +39,9 @@ export const ToastProvider: FC<ToastProviderProps> = ({ children }) => {
         {show && toastProps && (
           <StackedPortal id="toast" order={1} left={0} right={0} bottom={0} zIndex={10} safeAreaMode="margin">
             <Transition animation={{ opacity: show ? 1 : 0 }} duration={200} easing="easeOutQuad">
-              <Toast {...toastProps} />
+              <HStack mx={20} mb={[20, 0]} mt={[0, 16]} flexGrow={[1, 0]} alignment="center">
+                <Toast {...toastProps} />
+              </HStack>
             </Transition>
           </StackedPortal>
         )}
