50ae984ab5ae3e4152459ea699cef1c1a48e91ff
YimJiYoung <chiy323@gmail.com>
2022-10-05 11:57:40 +0900

    fix: set contentContainerStyle flexGlow 1 in ScrollView

--- packages/vibrant-core/src/lib/ScrollBox/ScrollBox.native.tsx
+++ packages/vibrant-core/src/lib/ScrollBox/ScrollBox.native.tsx
@@ -8,7 +8,18 @@ export const ScrollBox = styled(
   forwardRef<any, ScrollBoxProps & { style: any }>(({ style, overflowX, ...restProps }, ref) => {
     const { props, ...restStyle } = StyleSheet.flatten(style);
 
-    return <ScrollView ref={ref} style={restStyle} horizontal={overflowX === 'auto'} {...restProps} {...props} />;
+    return (
+      <ScrollView
+        ref={ref}
+        style={restStyle}
+        horizontal={overflowX === 'auto'}
+        contentContainerStyle={{
+          flexGrow: 1,
+        }}
+        {...restProps}
+        {...props}
+      />
+    );
   }),
   {
     shouldForwardProp,
