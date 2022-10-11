022d6c4950ea06fd0be15c0b51005f8fc92b8532
YimJiYoung <chiy323@gmail.com>
2022-10-11 16:30:05 +0900

    chore: lint

--- packages/vibrant-utils/src/lib/useControllableState/useControllableState.ts
+++ packages/vibrant-utils/src/lib/useControllableState/useControllableState.ts
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useState } from 'react';
+import { useCallback, useState } from 'react';
 import { isDefined } from '../isDefined';
 import { useCallbackRef } from '../useCallbackRef';
 
