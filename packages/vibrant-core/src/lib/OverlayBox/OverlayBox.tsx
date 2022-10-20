db5aa506a737c3fc911d1d6e7a09e2efa78609c0
YimJiYoung <chiy323@gmail.com>
2022-10-20 15:23:02 +0900

    refactor: remove as div

--- packages/vibrant-core/src/lib/OverlayBox/OverlayBox.tsx
+++ packages/vibrant-core/src/lib/OverlayBox/OverlayBox.tsx
@@ -32,9 +32,8 @@ export const OverlayBox = withOverlayBoxVariation(({ open, innerRef, onDismiss, 
 
   return (
     <PortalBox top={0} right={0} bottom={0} left={0}>
-      <PressableBox as="div" position="absolute" width="100%" height="100%" onClick={() => onDismiss?.()} />
+      <PressableBox position="absolute" width="100%" height="100%" onClick={() => onDismiss?.()} />
       <PressableBox
-        as="div"
         top={targetRect.y}
         left={targetRect.x}
         width={targetRect.width}
