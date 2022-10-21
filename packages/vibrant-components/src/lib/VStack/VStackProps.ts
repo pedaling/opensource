ae50556818d1b818b377af42497376cadadbdd78
lou-lee <lou@101.inc>
2022-10-21 14:28:44 +0900

    refactor: merge two types import statements

--- packages/vibrant-components/src/lib/VStack/VStackProps.ts
+++ packages/vibrant-components/src/lib/VStack/VStackProps.ts
@@ -1,7 +1,6 @@
 import type { ResponsiveValue } from '@vibrant-ui/core';
 import { withVariation } from '@vibrant-ui/core';
-import type { StackProps } from '../Stack';
-import type { Alignment } from '../Stack/StackProps';
+import type { Alignment, StackProps } from '../Stack';
 
 export type VStackProps = Omit<StackProps, 'alignHorizontal' | 'alignVertical' | 'direction'> & {
   alignHorizontal?: ResponsiveValue<Exclude<Alignment, 'space-between'>>;
