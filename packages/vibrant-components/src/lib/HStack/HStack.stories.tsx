26758220596bc846ee6f39fc422ecc6ae16fd682
lou(루) <105209178+lou-lee@users.noreply.github.com>
2022-10-21 14:34:15 +0900

    refactor: updates storybook stacks and exclude props (#366)
    * refactor: updates storybook stacks and exclude props
    
    * refactor: merge two types import statements

--- packages/vibrant-components/src/lib/HStack/HStack.stories.tsx
+++ packages/vibrant-components/src/lib/HStack/HStack.stories.tsx
@@ -12,10 +12,10 @@ export default {
 
 export const Basic: ComponentStory<typeof HStack> = props => (
   <HStack {...props} flex={1} width="100%" height="100%">
-    <Box width={100} height={100} backgroundColor="primary" />
-    <Box width={100} height={100} backgroundColor="primary" />
-    <Box width={200} height={100} backgroundColor="primary" />
-    <Box width={100} height={100} backgroundColor="primary" />
-    <Box width={100} height={100} backgroundColor="primary" />
+    <Box width={100} minHeight={100} backgroundColor="primary" />
+    <Box width={100} minHeight={100} backgroundColor="primary" />
+    <Box width={200} minHeight={100} backgroundColor="primary" />
+    <Box width={100} minHeight={100} backgroundColor="primary" />
+    <Box width={100} minHeight={100} backgroundColor="primary" />
   </HStack>
 );
--- packages/vibrant-components/src/lib/VStack/VStackProps.ts
+++ packages/vibrant-components/src/lib/VStack/VStackProps.ts
@@ -1,11 +1,10 @@
 import type { ResponsiveValue } from '@vibrant-ui/core';
 import { withVariation } from '@vibrant-ui/core';
-import type { StackProps } from '../Stack';
-import type { Alignment } from '../Stack/StackProps';
+import type { Alignment, StackProps } from '../Stack';
 
 export type VStackProps = Omit<StackProps, 'alignHorizontal' | 'alignVertical' | 'direction'> & {
   alignHorizontal?: ResponsiveValue<Exclude<Alignment, 'space-between'>>;
-  alignVertical?: ResponsiveValue<Alignment>;
+  alignVertical?: ResponsiveValue<Exclude<Alignment, 'stretch'>>;
 };
 
 export const withVStackVariation = withVariation<VStackProps>('VStack')();
