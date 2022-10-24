8b58da4a20778fae70619d2df2ccef2b9e833c63
peter <tbvjaos510@naver.com>
2022-10-24 10:57:08 +0900

    fix: 리뷰 반영
    Co-authored-by: Naki(나키) <nbsp1221@gmail.com>

--- jest.config.ts
+++ jest.config.ts
@@ -1,10 +1,8 @@
 import { getJestProjects } from '@nrwl/jest';
 
-let projects = getJestProjects();
-
-projects = projects.filter(project => !project.includes('vibrant-core'));
-
-projects = projects.concat('packages/vibrant-core/jest.config.web.ts', 'packages/vibrant-core/jest.config.native.ts');
+const projects = getJestProjects()
+  .filter(project => !project.includes('vibrant-core'))
+  .concat('packages/vibrant-core/jest.config.web.ts', 'packages/vibrant-core/jest.config.native.ts');
 
 export default {
   projects,
