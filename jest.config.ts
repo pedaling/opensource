2fdab76585e8b43320d467679b4e97626711f16d
peter <tbvjaos510@naver.com>
2022-10-24 11:00:32 +0900

    fix: jest.config.ts에 vibrant-core를 플랫폼별로 분기한다 (#368)
    * fix: jest.config.ts에 vibrant-core를 플랫폼별로 분기한다
    
    * fix: 리뷰 반영
    
    Co-authored-by: Naki(나키) <nbsp1221@gmail.com>
    
    Co-authored-by: Naki(나키) <nbsp1221@gmail.com>

--- jest.config.ts
+++ jest.config.ts
@@ -1,5 +1,9 @@
 import { getJestProjects } from '@nrwl/jest';
 
+const projects = getJestProjects()
+  .filter(project => !project.includes('vibrant-core'))
+  .concat('packages/vibrant-core/jest.config.web.ts', 'packages/vibrant-core/jest.config.native.ts');
+
 export default {
-  projects: getJestProjects(),
+  projects,
 };
