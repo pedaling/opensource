14493d0ef1c374ab48e18a13d87c9dbbf5f2ec47
Ann (앤) <33626219+jhn3981@users.noreply.github.com>
2022-10-26 14:12:34 +0900

    feat: add overflow prop to Paper component (#378)

--- packages/vibrant-components/src/lib/Paper/Paper.tsx
+++ packages/vibrant-components/src/lib/Paper/Paper.tsx
@@ -1,6 +1,6 @@
 import { Box } from '@vibrant-ui/core';
 import { withPaperVariation } from './PaperProps';
 
-export const Paper = withPaperVariation(({ innerRef, ...restProps }) => (
-  <Box ref={innerRef} overflow="hidden" {...restProps} />
+export const Paper = withPaperVariation(({ innerRef, overflow, ...restProps }) => (
+  <Box ref={innerRef} overflow={overflow ?? 'hidden'} {...restProps} />
 ));
