e1808931f8314c9abb89279c661ac06e40ef08db
lou-lee <lou@101.inc>
2022-10-28 14:13:57 +0900

    fix: fix the textarea padding input area

--- packages/vibrant-website/src/theme/Playground/index.js
+++ packages/vibrant-website/src/theme/Playground/index.js
@@ -38,7 +38,6 @@ function ThemedLiveEditor() {
       // otherwise dark prism theme is not applied
       key={String(isBrowser)}
       className={styles.playgroundEditor}
-      style={{ padding: '0.7rem' }}
     />
   );
 }
