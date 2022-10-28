6691587155088f95f0d1e042844ef7cef1109de0
Mia(임지영) <37496919+YimJiYoung@users.noreply.github.com>
2022-10-28 18:05:49 +0900

    feat: GhostButton 컴포넌트를 추가한다 (#389)
    * feat: add Ghost Button
    
    * chore: update stories.ts
    
    * fix: apply icon and text color
    
    * fix: set default value of disclosure none
    
    * fix: set default color to onView1
    
    * fix: fix icon import

---
+++ packages/vibrant-components/src/lib/GhostButton/GhostButton.stories.tsx
@@ -0,0 +1,17 @@
+import type { ComponentMeta, ComponentStory } from '@storybook/react';
+import { Icon } from '@vibrant-ui/icons';
+import { GhostButton } from './GhostButton';
+
+export default {
+  title: 'GhostButton',
+  component: GhostButton,
+  args: {
+    kind: 'primary',
+    size: 'md',
+    IconComponent: Icon.Photo.Regular,
+    children: 'click me',
+    disclosure: 'right',
+  },
+} as ComponentMeta<typeof GhostButton>;
+
+export const Basic: ComponentStory<typeof GhostButton> = props => <GhostButton {...props} />;
