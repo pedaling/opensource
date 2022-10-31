eeff2493d14acec5fcd045692c7fae4c37cffad4
peter <peter@101.inc>
2022-10-31 11:59:14 +0900

    fix: macos로 변경해봄

--- jest.preset.js
+++ jest.preset.js
@@ -4,6 +4,7 @@ module.exports = {
   ...nxPreset,
   runner: '@kayahr/jest-electron-runner',
   testEnvironment: '@kayahr/jest-electron-runner/environment',
+  reporters: ['default', 'github-actions'],
   testTimeout: 10000,
   moduleNameMapper: {
     // Force module uuid to resolve with the CJS entry point, because Jest does not support package.json.exports. See https://github.com/uuidjs/uuid/issues/451
