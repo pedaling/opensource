889f8020166583a1b819792912fa6d7aab4e12eb
lou(루) <105209178+lou-lee@users.noreply.github.com>
2022-11-16 10:46:58 +0900

    feat: update callout new style (#423)

--- packages/vibrant-components/src/lib/Callout/Callout.tsx
+++ packages/vibrant-components/src/lib/Callout/Callout.tsx
@@ -1,6 +1,6 @@
 import { Box, Text } from '@vibrant-ui/core';
+import { ContainedButton } from '../ContainedButton';
 import { HStack } from '../HStack';
-import { Pressable } from '../Pressable';
 import { Title } from '../Title';
 import { VStack } from '../VStack';
 import { withCalloutVariation } from './CalloutProps';
@@ -14,41 +14,39 @@ export const Callout = withCalloutVariation(
       borderWidth={1}
       borderColor="outline1"
       borderRadius={2}
+      p={15}
     >
-      <VStack p={16}>
-        <HStack spacing={6}>
-          <Box mt={2}>
-            <IconComponent.Fill fill={fontColor} size={16} />
-          </Box>
-          <Title level={6} weight="bold" color={fontColor} overflowWrap="anywhere">
-            {title}
-          </Title>
-        </HStack>
-        {contents ? (
-          <Text mt={8} mb={contents ? 2 : 0} lineHeight={18} fontSize={14} fontWeight="regular" overflowWrap="anywhere">
-            {contents}
-          </Text>
-        ) : (
-          renderContents?.()
-        )}
-        {buttonText ? (
-          <Box mt={12} alignSelf="flex-end">
-            <Pressable
-              onClick={onButtonClick}
-              interactions={['focus', 'active']}
-              backgroundColor="surface1"
-              borderRadius={2}
-              overlayColor="onView1"
-              py={10}
-              px={14}
-            >
-              <Text lineHeight={18} fontSize={14} fontWeight="bold" color="onView1">
-                {buttonText}
-              </Text>
-            </Pressable>
-          </Box>
-        ) : null}
-      </VStack>
+      <HStack spacing={6}>
+        <Box mt={1}>
+          <IconComponent.Fill fill={fontColor} size={16} />
+        </Box>
+        <Title level={7} weight="bold" color={fontColor} overflowWrap="anywhere">
+          {title}
+        </Title>
+      </HStack>
+      {contents ? (
+        <Text
+          color="onView1"
+          mt={8}
+          mb={contents ? 2 : 0}
+          lineHeight={18}
+          fontSize={14}
+          fontWeight="regular"
+          flex={1}
+          overflowWrap="anywhere"
+        >
+          {contents}
+        </Text>
+      ) : (
+        renderContents?.()
+      )}
+      {buttonText ? (
+        <VStack alignHorizontal="end" mt={12}>
+          <ContainedButton onClick={onButtonClick} size="md" kind="tertiary">
+            {buttonText}
+          </ContainedButton>
+        </VStack>
+      ) : null}
     </Box>
   )
 );
