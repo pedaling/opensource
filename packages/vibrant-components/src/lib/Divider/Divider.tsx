25a393e5dd759d6bc7720bd61321d00ff7336cc8
YimJiYoung <chiy323@gmail.com>
2022-11-16 21:02:35 +0900

    fix: remove flex none in Divider

--- packages/vibrant-components/src/lib/Divider/Divider.tsx
+++ packages/vibrant-components/src/lib/Divider/Divider.tsx
@@ -1,4 +1,6 @@
 import { Box } from '@vibrant-ui/core';
 import { withDividerVariation } from './DividerProps';
 
-export const Divider = withDividerVariation(({ ...props }) => <Box flex="none" borderWidth={0} {...props} />);
+export const Divider = withDividerVariation(({ ...props }) => (
+  <Box flexGrow={0} flexShrink={0} flexBasis="auto" borderWidth={0} {...props} />
+));
