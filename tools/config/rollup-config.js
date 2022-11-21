1565b003c4e444a3d96db85d2d84178ef910ee84
zlrlo <easy10612@gmail.com>
2022-11-21 17:35:43 +0900

    fix: vibrant-core preserveModules 설정 제외

--- tools/config/rollup-config.js
+++ tools/config/rollup-config.js
@@ -1,12 +1,13 @@
 const multiInput = require('rollup-plugin-multi-input').default;
 const rollupTypescript = require('rollup-plugin-typescript2');
+const { visualizer } = require('rollup-plugin-visualizer');
 
 module.exports = function rollupConfig(config, options) {
   if (!options.watch) {
     process.env.NODE_ENV = 'production';
   }
 
-  const { moduleSuffixes = [''], typeCheck = true, main } = options;
+  const { moduleSuffixes = [''], typeCheck = true, main, analyzer, disablePreserveModules } = options;
 
   const tsPluginIndex = config.plugins.findIndex(plugin => plugin.name === 'rpt2');
 
@@ -33,12 +34,23 @@ module.exports = function rollupConfig(config, options) {
     config.plugins.splice(0, 0, multiInput({ relative: options.entryRoot }));
   }
 
+  if (analyzer) {
+    config.plugins.push(
+      visualizer({
+        gzipSize: true,
+        open: true,
+      })
+    );
+  }
+
   if (options.forceOutputJsExtension) {
     config.output.entryFileNames = '[name].js';
     config.output.chunkFileNames = '[name].js';
   }
 
-  config.output.preserveModules = true;
+  if (!disablePreserveModules) {
+    config.output.preserveModules = true;
+  }
 
   return config;
 };
