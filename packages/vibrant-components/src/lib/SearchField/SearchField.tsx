8fb214987bcac40d1e82c49c4c1456b2096f3944
lou-lee <lou@101.inc>
2022-11-23 17:07:42 +0900

    feat: add default value

--- packages/vibrant-components/src/lib/SearchField/SearchField.tsx
+++ packages/vibrant-components/src/lib/SearchField/SearchField.tsx
@@ -5,62 +5,64 @@ import { Icon } from '@vibrant-ui/icons';
 import { Pressable } from '../Pressable';
 import { withSearchFieldVariation } from './SearchFieldProps';
 
-export const SearchField = withSearchFieldVariation(({ defaultValue, onValueChange, kind, ...restProps }) => {
-  const [value, setValue] = useState(defaultValue ?? '');
-  const inputRef = useRef<TextInputRef>(null);
+export const SearchField = withSearchFieldVariation(
+  ({ defaultValue, onValueChange, kind = 'default', ...restProps }) => {
+    const [value, setValue] = useState(defaultValue ?? '');
+    const inputRef = useRef<TextInputRef>(null);
 
-  useEffect(() => {
-    setValue(defaultValue ?? '');
-  }, [defaultValue]);
+    useEffect(() => {
+      setValue(defaultValue ?? '');
+    }, [defaultValue]);
 
-  return (
-    <Box position="relative" width="100%">
-      <TextInput
-        ref={inputRef}
-        type="text"
-        py={10}
-        pl={16}
-        pr={48}
-        backgroundColor={kind === 'default' ? 'surface1' : 'transparent'}
-        typography="body2"
-        color="onView1"
-        placeholderColor="onView3"
-        defaultValue={value}
-        onValueChange={({ value, prevent }) => {
-          let isPrevented = false;
+    return (
+      <Box position="relative" width="100%">
+        <TextInput
+          ref={inputRef}
+          type="text"
+          py={10}
+          pl={16}
+          pr={48}
+          backgroundColor={kind === 'default' ? 'surface1' : 'transparent'}
+          typography="body2"
+          color="onView1"
+          placeholderColor="onView3"
+          defaultValue={value}
+          onValueChange={({ value, prevent }) => {
+            let isPrevented = false;
 
-          onValueChange?.({
-            value,
-            prevent: () => {
-              isPrevented = true;
+            onValueChange?.({
+              value,
+              prevent: () => {
+                isPrevented = true;
 
-              prevent();
-            },
-          });
+                prevent();
+              },
+            });
 
-          if (!isPrevented) {
-            setValue(value);
-          }
-        }}
-        {...restProps}
-      />
-      <Box
-        as="span"
-        position="absolute"
-        top={9}
-        right={16}
-        zIndex={1}
-        cursor="text"
-        onClick={() => inputRef.current?.focus()}
-      >
-        {value.length > 0 ? (
-          <Pressable onClick={() => inputRef.current?.clear()}>
-            <Icon.CloseCircle.Fill size={20} fill="onView2" />
-          </Pressable>
-        ) : (
-          <Icon.Search.Regular size={20} fill="onView1" />
-        )}
+            if (!isPrevented) {
+              setValue(value);
+            }
+          }}
+          {...restProps}
+        />
+        <Box
+          as="span"
+          position="absolute"
+          top={9}
+          right={16}
+          zIndex={1}
+          cursor="text"
+          onClick={() => inputRef.current?.focus()}
+        >
+          {value.length > 0 ? (
+            <Pressable onClick={() => inputRef.current?.clear()}>
+              <Icon.CloseCircle.Fill size={20} fill="onView2" />
+            </Pressable>
+          ) : (
+            <Icon.Search.Regular size={20} fill="onView1" />
+          )}
+        </Box>
       </Box>
-    </Box>
-  );
-});
+    );
+  }
+);
