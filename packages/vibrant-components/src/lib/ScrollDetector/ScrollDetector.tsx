3466fb2f47fad97282c31240e43814f5a0efe94f
lou-lee <lou@101.inc>
2022-11-29 14:41:36 +0900

    feat: update scrolldetector return scrollposition

--- packages/vibrant-components/src/lib/ScrollDetector/ScrollDetector.tsx
+++ packages/vibrant-components/src/lib/ScrollDetector/ScrollDetector.tsx
@@ -1,15 +1,22 @@
 import type { ReactElement } from 'react';
+import { useEffect, useState } from 'react';
 import { useScroll } from '@vibrant-ui/core';
-import type { EventListenerCallback, ScrollDirection } from '@vibrant-ui/core';
+import type { ScrollDirection } from '@vibrant-ui/core';
 type ScrollDetectorProps = {
-  children: (_: {
-    scrollDirection: ScrollDirection;
-    addEventListener: (callback: EventListenerCallback) => void;
-  }) => ReactElement;
+  children: (_: { scrollDirection: ScrollDirection; scrollPosition: number }) => ReactElement;
 };
 
 export const ScrollDetector = ({ children }: ScrollDetectorProps) => {
+  const [scrollPosition, setScrollPosition] = useState(0);
   const { scrollDirection, addEventListener } = useScroll();
 
-  return children ? children({ scrollDirection, addEventListener }) : null;
+  useEffect(() => {
+    const cleanEventListener = addEventListener(({ scrollPosition }) => {
+      setScrollPosition(scrollPosition);
+    });
+
+    return cleanEventListener;
+  }, [addEventListener]);
+
+  return children ? children({ scrollDirection, scrollPosition }) : null;
 };
