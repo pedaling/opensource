60c9b893cd67a6e8d9b678c96b51cf9c5f14bdb8
Ann (앤) <33626219+jhn3981@users.noreply.github.com>
2022-11-30 17:05:15 +0900

    fix: render children in ContentArea (#456)

--- packages/vibrant-components/src/lib/ContentArea/ContentArea.tsx
+++ packages/vibrant-components/src/lib/ContentArea/ContentArea.tsx
@@ -2,14 +2,16 @@ import { VStack } from '@vibrant-ui/components';
 import { useCurrentTheme } from '@vibrant-ui/core';
 import { withContentAreaVariation } from './ContentAreaProps';
 
-export const ContentArea = withContentAreaVariation(({ padding = true }) => {
+export const ContentArea = withContentAreaVariation(({ padding = true, children }) => {
   const {
     theme: { contentArea },
   } = useCurrentTheme();
 
   return (
     <VStack alignHorizontal="center" width="100%">
-      <VStack maxWidth={contentArea.maxWidth} width="100%" px={padding ? contentArea.padding : 0} />
+      <VStack maxWidth={contentArea.maxWidth} width="100%" px={padding ? contentArea.padding : 0}>
+        {children}
+      </VStack>
     </VStack>
   );
 });
