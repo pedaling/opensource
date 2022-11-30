9c4e722483c2f4c580e97c1bd20567e9cb4cef20
민 <min@101.inc>
2022-11-30 10:50:26 +0900

    fix: remove kind prop

--- packages/vibrant-components/src/lib/TableSearchField/TableSearchFieldProps.ts
+++ packages/vibrant-components/src/lib/TableSearchField/TableSearchFieldProps.ts
@@ -4,7 +4,6 @@ import type { BaseInputProps } from '../../types';
 type TableSearchFieldProps = BaseInputProps<string> & {
   autoFocus?: boolean;
   placeholder?: string;
-  kind?: 'default' | 'transparent';
   onSubmit?: (value: string) => void;
   width: number;
 };
