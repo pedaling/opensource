9eba255ede2f77c22099542774fc0c0b805614af
민 <min@101.inc>
2022-12-01 20:37:49 +0900

    fix: modify the code to match the changed factor

--- packages/vibrant-components/src/lib/pageSizeSection/PageSizeSection.tsx
+++ packages/vibrant-components/src/lib/pageSizeSection/PageSizeSection.tsx
@@ -2,20 +2,17 @@ import { useState } from 'react';
 import { Body, Dropdown, GhostButton, HStack, Pressable, VStack } from '@vibrant-ui/components';
 import { Text, useConfig } from '@vibrant-ui/core';
 import { Icon } from '@vibrant-ui/icons';
-import type { PageSizeOption } from './PageSizeSectionProps';
 import { withPageSizeSectionVariation } from './PageSizeSectionProps';
 
-const DEFAULT_PAGE_SIZE_OPTION = { label: '', value: 0, onClick: () => {}, initial: true };
-
-export const PageSizeSection = withPageSizeSectionVariation(({ pageSizeOptions }) => {
+export const PageSizeSection = withPageSizeSectionVariation(({ pageSizeOptions, onClickPageSize }) => {
   const {
     translations: { pageSizeSection: pageSizeSectionTranslation },
   } = useConfig();
-  const defaultPageSizeOption = pageSizeOptions.find(pageSizeOption => pageSizeOption.initial);
-  const [selectedOption, setSelectedOption] = useState(defaultPageSizeOption || DEFAULT_PAGE_SIZE_OPTION);
+  const defaultPageSizeOption = pageSizeOptions.find(pageSizeOption => pageSizeOption);
+  const [selectedPageSize, setSelectedPageSize] = useState(defaultPageSizeOption || 10);
 
-  const selectPageSizeOption = (pageSizeOption: PageSizeOption) => {
-    setSelectedOption(pageSizeOption);
+  const selectPageSizeOption = (pageSize: number) => {
+    setSelectedPageSize(pageSize);
   };
 
   return (
@@ -28,27 +25,27 @@ export const PageSizeSection = withPageSizeSectionVariation(({ pageSizeOptions }
         position="bottom-end"
         renderOpener={({ open }) => (
           <GhostButton size="md" color="onView1" disclosure={true} onClick={open}>
-            {selectedOption.value}
+            {selectedPageSize}
           </GhostButton>
         )}
         renderContents={({ close }) => (
           <VStack spacing={16}>
-            {pageSizeOptions.map(({ label, value, onClick, initial }, index) => (
+            {pageSizeOptions.map((pageSize: number) => (
               <Pressable
                 onClick={() => {
-                  onClick?.(value);
+                  selectPageSizeOption(pageSize);
 
-                  selectPageSizeOption({ label, value, onClick, initial });
+                  onClickPageSize(pageSize);
 
                   close();
                 }}
-                key={`${value}_${index}`}
+                key={pageSize}
               >
                 <HStack alignHorizontal="space-between" px={16}>
                   <Body level={1} color="onView1" weight="medium">
-                    {label}
+                    {pageSize}
                   </Body>
-                  {selectedOption.value === value && <Icon.Check.Fill size={18} />}
+                  {selectedPageSize === pageSize && <Icon.Check.Fill size={18} />}
                 </HStack>
               </Pressable>
             ))}
