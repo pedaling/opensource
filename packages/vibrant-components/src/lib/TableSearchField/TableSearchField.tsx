c411a7113ac63996bee8ab7d36fc88198f748cf9
민 <min@101.inc>
2022-12-01 18:42:01 +0900

    fix: remove unnecessary box component

--- packages/vibrant-components/src/lib/TableSearchField/TableSearchField.tsx
+++ packages/vibrant-components/src/lib/TableSearchField/TableSearchField.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useRef, useState } from 'react';
 import type { TextInputRef } from '@vibrant-ui/core';
-import { Box, TextInput } from '@vibrant-ui/core';
+import { TextInput } from '@vibrant-ui/core';
 import { withTableSearchFieldVariation } from './TableSearchFieldProps';
 
 export const TableSearchField = withTableSearchFieldVariation(
@@ -13,44 +13,40 @@ export const TableSearchField = withTableSearchFieldVariation(
     }, [defaultValue]);
 
     return (
-      <Box
-        position="relative"
+      <TextInput
         maxWidth={320}
         borderRadius={1}
         borderWidth={1}
         borderStyle="solid"
         borderColor="outline1"
         width={width}
-      >
-        <TextInput
-          ref={inputRef}
-          type="text"
-          px={15}
-          py={8}
-          backgroundColor="transparent"
-          typography="body2"
-          color="onView1"
-          placeholderColor="onView3"
-          defaultValue={value}
-          onValueChange={({ value, prevent }) => {
-            let isPrevented = false;
+        ref={inputRef}
+        type="text"
+        px={15}
+        py={8}
+        backgroundColor="transparent"
+        typography="body2"
+        color="onView1"
+        placeholderColor="onView3"
+        defaultValue={value}
+        onValueChange={({ value, prevent }) => {
+          let isPrevented = false;
 
-            onValueChange?.({
-              value,
-              prevent: () => {
-                isPrevented = true;
+          onValueChange?.({
+            value,
+            prevent: () => {
+              isPrevented = true;
 
-                prevent();
-              },
-            });
+              prevent();
+            },
+          });
 
-            if (!isPrevented) {
-              setValue(value);
-            }
-          }}
-          {...restProps}
-        />
-      </Box>
+          if (!isPrevented) {
+            setValue(value);
+          }
+        }}
+        {...restProps}
+      />
     );
   }
 );
