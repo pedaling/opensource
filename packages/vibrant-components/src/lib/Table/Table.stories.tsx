a9b4e65706d4ece25aa888b8f9622371bdd2b274
YimJiYoung <chiy323@gmail.com>
2022-12-21 17:08:29 +0900

    chore: add Table story

--- packages/vibrant-components/src/lib/Table/Table.stories.tsx
+++ packages/vibrant-components/src/lib/Table/Table.stories.tsx
@@ -5,7 +5,6 @@ import { Callout } from '../Callout';
 import { OutlinedButton } from '../OutlinedButton';
 import { Paper } from '../Paper';
 import { Table, useTable } from './Table';
-import type { SortDirection } from './TableSortButton';
 
 type Data = {
   name: string;
@@ -47,8 +46,9 @@ export default {
         />
       </Paper>
     ),
-    onSort: (dataKey: string, direction: SortDirection) => console.log(dataKey, direction),
-    onRow: (row: Data) => console.log(row),
+    onRow: {
+      onClick: (row: Data) => console.log(row),
+    },
     emptyText: 'No Data',
     emptyImage: 'https://cdn.class101.net/images/a097865b-683e-4386-a6a3-1fa27d1463d0',
     disabledRowKey: 'Cupcake',
@@ -57,11 +57,12 @@ export default {
 
 export const Basic: ComponentStory<typeof Table> = props => (
   <Box p={20} width="100%">
-    <Table {...props}>
+    <Table {...props} selectable={false} renderExpanded={undefined}>
       <Table.Column<Data>
         key="name"
         dataKey="name"
         renderHeader={() => <OutlinedButton size="sm">이름 수정</OutlinedButton>}
+        textAlign="center"
       />
       <Table.Column<Data> key="calories" dataKey="calories" title="calories" />
       <Table.Column<Data>
@@ -71,15 +72,109 @@ export const Basic: ComponentStory<typeof Table> = props => (
         description="abc"
         sortable={true}
         defaultSortDirection="asc"
-        selectable={true}
-        onCell={{
-          onClick: row => console.log(row),
-          onCopy: row => console.log(row),
-        }}
         formatData={({ fat }) => `${fat} kcal`}
       />
       <Table.Column<Data> key="carbs" dataKey="carbs" title="carbs" />
       <Table.Column<Data> key="protein" dataKey="protein" title="protein" />
+      <Table.Column<Data>
+        key="Edit"
+        title=""
+        width={120}
+        renderCell={() => <OutlinedButton size="sm">수정</OutlinedButton>}
+      />
+    </Table>
+  </Box>
+);
+
+export const RowSelectable: ComponentStory<typeof Table> = props => (
+  <Box p={20} width="100%">
+    <Table {...props}>
+      <Table.Column<Data>
+        key="name"
+        dataKey="name"
+        renderHeader={() => <OutlinedButton size="sm">이름 수정</OutlinedButton>}
+        textAlign="center"
+      />
+      <Table.Column<Data> key="calories" dataKey="calories" title="calories" />
+      <Table.Column<Data>
+        key="fat"
+        dataKey="fat"
+        title="fat"
+        description="abc"
+        sortable={true}
+        defaultSortDirection="asc"
+        formatData={({ fat }) => `${fat} kcal`}
+      />
+      <Table.Column<Data> key="carbs" dataKey="carbs" title="carbs" />
+      <Table.Column<Data> key="protein" dataKey="protein" title="protein" />
+      <Table.Column<Data>
+        key="Edit"
+        title=""
+        width={120}
+        renderCell={() => <OutlinedButton size="sm">수정</OutlinedButton>}
+      />
+    </Table>
+  </Box>
+);
+
+export const cellSelectable: ComponentStory<typeof Table> = props => (
+  <Box p={20} width="100%">
+    <Table {...props} selectable={false}>
+      <Table.Column<Data>
+        key="name"
+        dataKey="name"
+        renderHeader={() => <OutlinedButton size="sm">이름 수정</OutlinedButton>}
+        textAlign="center"
+        selectable={true}
+        onCell={{
+          onClick: row => console.log('clicked', row),
+          onCopy: row => console.log(row),
+        }}
+      />
+      <Table.Column<Data>
+        key="calories"
+        dataKey="calories"
+        title="calories"
+        selectable={true}
+        onCell={{
+          onClick: row => console.log('clicked', row),
+          onCopy: row => console.log('copied', row),
+        }}
+      />
+      <Table.Column<Data>
+        key="fat"
+        dataKey="fat"
+        title="fat"
+        description="abc"
+        sortable={true}
+        defaultSortDirection="asc"
+        formatData={({ fat }) => `${fat} kcal`}
+        selectable={true}
+        onCell={{
+          onClick: row => console.log('clicked', row),
+          onCopy: row => console.log('copied', row),
+        }}
+      />
+      <Table.Column<Data>
+        key="carbs"
+        dataKey="carbs"
+        title="carbs"
+        selectable={true}
+        onCell={{
+          onClick: row => console.log('clicked', row),
+          onCopy: row => console.log('copied', row),
+        }}
+      />
+      <Table.Column<Data>
+        key="protein"
+        dataKey="protein"
+        title="protein"
+        selectable={true}
+        onCell={{
+          onClick: row => console.log('clicked', row),
+          onCopy: row => console.log('copied', row),
+        }}
+      />
       <Table.Column<Data>
         key="Edit"
         title=""
