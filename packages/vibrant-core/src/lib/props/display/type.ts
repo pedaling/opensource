61f632c4dc0797662faf258dd9b3fbe0bf11bf56
YimJiYoung <chiy323@gmail.com>
2022-12-23 16:47:02 +0900

    chore: fix table display type

--- packages/vibrant-core/src/lib/props/display/type.ts
+++ packages/vibrant-core/src/lib/props/display/type.ts
@@ -2,7 +2,8 @@ import type { OpacityToken } from '@vibrant-ui/theme';
 import type { ResponsiveValue } from '../../../types';
 
 export type DisplaySystemProps = {
-  display?: ResponsiveValue<'flex' | 'inline-flex' | 'none' | 'table-cell' | 'table-row' | 'table'>;
+  // MEMO: 'table', 'table-row', 'table' only works in web
+  display?: ResponsiveValue<'flex' | 'inline-flex' | 'none' | 'table-cell' | 'table-layout' | 'table-row'>;
   visibility?: ResponsiveValue<'hidden' | 'inherit' | 'visible'>;
   opacity?: ResponsiveValue<OpacityToken | number>;
   hidden?: ResponsiveValue<boolean>;
