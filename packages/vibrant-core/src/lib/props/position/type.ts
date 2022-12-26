00acbc6667a8ceb461e30d26dd5afc03df969f9e
YimJiYoung <chiy323@gmail.com>
2022-12-26 20:52:50 +0900

    feat: add position web_static

--- packages/vibrant-core/src/lib/props/position/type.ts
+++ packages/vibrant-core/src/lib/props/position/type.ts
@@ -1,7 +1,7 @@
 import type { ResponsiveValue } from '../../../types';
 
 export type PositionSystemProps = {
-  position?: ResponsiveValue<'absolute' | 'fixed' | 'relative' | 'web_sticky'>;
+  position?: ResponsiveValue<'absolute' | 'fixed' | 'relative' | 'web_static' | 'web_sticky'>;
   top?: ResponsiveValue<number | string>;
   right?: ResponsiveValue<number | string>;
   bottom?: ResponsiveValue<number | string>;
