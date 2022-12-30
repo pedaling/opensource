a29241c1ba03c6cfeb2d2f7ff2e181711741fed9
YimJiYoung <chiy323@gmail.com>
2022-12-30 17:45:31 +0900

    refactor: update TableHeaderButton prop

--- packages/vibrant-components/src/lib/TableHeader/TableHeaderButton/TableHeaderButtonProps.ts
+++ packages/vibrant-components/src/lib/TableHeader/TableHeaderButton/TableHeaderButtonProps.ts
@@ -2,9 +2,12 @@ import { withVariation } from '@vibrant-ui/core';
 import type { ContainedButtonProps } from '../../ContainedButton';
 import type { OutlinedButtonProps } from '../../OutlinedButton/OutlinedButtonProps';
 
-export type TableHeaderButtonProps = Omit<ContainedButtonProps, 'full' | 'kind' | 'size'> &
-  Omit<OutlinedButtonProps, 'full' | 'size'> & {
-    kind: 'outlined' | 'primary' | 'secondary' | 'tertiary';
-  };
+export type TableHeaderButtonProps =
+  | ({
+      kind: 'outlined';
+    } & Omit<OutlinedButtonProps, 'full' | 'size'>)
+  | ({
+      kind: 'primary' | 'secondary' | 'tertiary';
+    } & Omit<ContainedButtonProps, 'full' | 'kind' | 'size'>);
 
 export const withTableHeaderButtonVariation = withVariation<TableHeaderButtonProps>('TableHeaderButton')();
