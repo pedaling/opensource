268f12c5dc86dcaa707378e69052648db5071829
YimJiYoung <chiy323@gmail.com>
2023-01-02 19:42:35 +0900

    feat: add ContainedButton size constant

---
+++ packages/vibrant-components/src/lib/ContainedButton/constants/size.ts
@@ -0,0 +1,38 @@
+export const sizeVariation = {
+  sm: {
+    typography: 'body4',
+    py: 7,
+    px: 8,
+    spinnerSize: 'sm',
+    iconSize: 16,
+    disclosureSize: 16,
+    contentsSpacing: 0,
+  },
+  md: {
+    typography: 'body2',
+    py: 10,
+    px: 10,
+    spinnerSize: 'md',
+    iconSize: 18,
+    disclosureSize: 16,
+    contentsSpacing: 0,
+  },
+  lg: {
+    typography: 'body2',
+    py: 13,
+    px: 12,
+    spinnerSize: 'md',
+    iconSize: 18,
+    disclosureSize: 16,
+    contentsSpacing: 0,
+  },
+  xl: {
+    typography: 'body1',
+    py: 15,
+    px: 14,
+    spinnerSize: 'md',
+    iconSize: 20,
+    disclosureSize: 18,
+    contentsSpacing: 2,
+  },
+} as const;
