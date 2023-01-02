6febb853191728777c331432f0bd2586b6d260f4
lou-lee <lou@101.inc>
2023-01-02 14:05:54 +0900

    fix: no shrink even if the position is right side

--- packages/vibrant-components/src/lib/Tooltip/Tooltip.tsx
+++ packages/vibrant-components/src/lib/Tooltip/Tooltip.tsx
@@ -92,6 +92,7 @@ export const Tooltip = withTooltipVariation(
     }>({ x: 0, y: 0 });
     const openerRef = useRef<HTMLElement>(null);
     const targetRef = useRef<HTMLElement>();
+    const [width, setWidth] = useState(0);
     const timerRef = useRef<ReturnType<typeof setTimeout>>();
     const {
       theme: { zIndex },
@@ -112,6 +113,8 @@ export const Tooltip = withTooltipVariation(
       } = getOffsetAvoidingOverflowByPosition(openerRect, targetRect, position, offset);
 
       setOffset({ x, y, left: leftOffset, right: rightOffset });
+
+      setWidth(targetRect.width);
 
       setIsMounted(true);
     }, [offset, position]);
@@ -182,6 +185,7 @@ export const Tooltip = withTooltipVariation(
             }
           >
             <Box
+              width={width}
               maxWidth={maxWidth}
               position="fixed"
               py={6}
