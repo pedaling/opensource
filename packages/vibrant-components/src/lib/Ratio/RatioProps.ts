547fc72f24dd19ba28981fcb85174543674b3939
YimJiYoung <chiy323@gmail.com>
2023-01-05 17:05:55 +0900

    feat: add ref prop in Ratio

--- packages/vibrant-components/src/lib/Ratio/RatioProps.ts
+++ packages/vibrant-components/src/lib/Ratio/RatioProps.ts
@@ -1,9 +1,11 @@
+import type { Ref } from 'react';
 import type { ReactElementChildren, ResponsiveValue, SizingSystemProps } from '@vibrant-ui/core';
 import { propVariant, withVariation } from '@vibrant-ui/core';
 
 export type RatioProps = Pick<SizingSystemProps, 'maxWidth' | 'minWidth' | 'width'> & {
   ratio: ResponsiveValue<number>;
   children: ReactElementChildren;
+  ref?: Ref<any>;
 };
 
 export const withRatioVariation = withVariation<RatioProps>('Ratio')(
