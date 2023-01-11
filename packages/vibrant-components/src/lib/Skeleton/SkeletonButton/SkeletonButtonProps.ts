136fb7ed4ff1897eb7e96fad842aedc4bbad68c0
Mia(임지영) <37496919+YimJiYoung@users.noreply.github.com>
2023-01-11 17:33:14 +0900

    fix: Skeleton QA 사항을 반영한다 (#524)
    * fix: fix SkeletonMotion duration and easing
    
    * fix: set last line width 40% in SkeletonText
    
    * refactor: change ratio type to number in SkeletonImage
    
    * refactor: change 100% to  percentage type
    
    * feat: add maxWidth prop to Skeleton
    
    * feat: add xs size to SkeletonAvatar
    
    * docs: update skeleton doc
    
    * docs: update skeleton document
    
    * docs: add button link in Skeleton document

--- packages/vibrant-components/src/lib/Skeleton/SkeletonButton/SkeletonButtonProps.ts
+++ packages/vibrant-components/src/lib/Skeleton/SkeletonButton/SkeletonButtonProps.ts
@@ -4,7 +4,8 @@ import { sizeVariation } from '../../ContainedButton';
 
 export type SkeletonButtonProps = {
   size: ResponsiveValue<'lg' | 'md' | 'sm' | 'xl'>;
-  width?: ResponsiveValue<number | '100%'>;
+  width?: ResponsiveValue<number | `${number}%`>;
+  maxWidth?: ResponsiveValue<number | `${number}%`>;
 };
 
 export const withSkeletonButtonVariation = withVariation<SkeletonButtonProps>('SkeletonButton')(
--- packages/vibrant-components/src/lib/Skeleton/SkeletonProps.ts
+++ packages/vibrant-components/src/lib/Skeleton/SkeletonProps.ts
@@ -2,8 +2,9 @@ import type { BorderSystemProps, ResponsiveValue } from '@vibrant-ui/core';
 import { withVariation } from '@vibrant-ui/core';
 
 export type SkeletonProps = Pick<BorderSystemProps, 'borderRadiusLevel'> & {
-  width: ResponsiveValue<number | '100%'>;
-  height: ResponsiveValue<number | '100%'>;
+  width: ResponsiveValue<number | `${number}%`>;
+  height: ResponsiveValue<number | `${number}%`>;
+  maxWidth?: ResponsiveValue<number | `${number}%`>;
 };
 
 export const withSkeletonVariation = withVariation<SkeletonProps>('Skeleton')();
