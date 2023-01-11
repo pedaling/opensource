c6bdff38364ea1bb3396dc95e7383e0abf9f60bc
lou-lee <lou@101.inc>
2023-01-11 18:27:07 +0900

    feat: add reponsive values into Image component

--- packages/vibrant-core/src/lib/Image/Image.native.tsx
+++ packages/vibrant-core/src/lib/Image/Image.native.tsx
@@ -4,22 +4,51 @@ import { useConfig } from '../ConfigProvider';
 import { ExternalComponent } from '../ExternalComponent';
 import { withImageVariation } from './ImageProps';
 
-export const Image = withImageVariation(({ src, alt, ...props }) => {
+export const Image = withImageVariation(({ src, alt, loading, objectFit, ...props }) => {
   const {
     dependencies: { image },
   } = useConfig();
 
-  if (image) {
-    return <ExternalComponent name="image" src={src} {...props} />;
+  if (Array.isArray(src)) {
+    const basicDisplay = new Array(src.length).fill('none');
+
+    return (
+      <>
+        {src.map((currentSrc, idx) => {
+          const display = [...basicDisplay];
+
+          display[idx] = 'flex';
+
+          if (image) {
+            <Box display={display}>
+              <ExternalComponent name="image" src={currentSrc} loading={loading} objectFit={objectFit} />
+            </Box>;
+          }
+
+          return (
+            <Box
+              base={NativeImage}
+              display={display}
+              key={currentSrc}
+              as="img"
+              source={{
+                uri: currentSrc,
+              }}
+              {...props}
+            />
+          );
+        })}
+      </>
+    );
   }
 
   return (
     <Box
       base={NativeImage}
+      as="img"
       source={{
         uri: src,
       }}
-      accessibilityLabel={alt}
       {...props}
     />
   );
--- packages/vibrant-core/src/lib/Image/Image.tsx
+++ packages/vibrant-core/src/lib/Image/Image.tsx
@@ -1,16 +1,33 @@
 import { Box } from '../Box';
 import { useConfig } from '../ConfigProvider';
-import { ExternalComponent } from '../ExternalComponent';
 import { withImageVariation } from './ImageProps';
 
-export const Image = withImageVariation(({ src, alt, ...props }) => {
+export const Image = withImageVariation(({ src, alt, loading, objectFit, ...props }) => {
   const {
     dependencies: { image },
   } = useConfig();
 
-  if (image) {
-    return <ExternalComponent name="image" src={src} {...props} />;
+  const Component = image ?? Box;
+
+  if (Array.isArray(src)) {
+    const basicDisplay = new Array(src.length).fill('none');
+
+    return (
+      <>
+        {src.map((currentSrc, idx) => {
+          const display = [...basicDisplay];
+
+          display[idx] = 'flex';
+
+          return (
+            <Box key={currentSrc} display={display}>
+              <Component as="img" src={currentSrc} objectFit={objectFit} loading={loading} {...props} />
+            </Box>
+          );
+        })}
+      </>
+    );
   }
 
-  return <Box as="img" src={src} alt={alt} {...props} />;
+  return <Component as="img" src={src} alt={alt} {...props} />;
 });
