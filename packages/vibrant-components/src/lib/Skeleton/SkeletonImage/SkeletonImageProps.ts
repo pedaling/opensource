6a0c82ed57820a16fdd9f6af103aede4a7792d32
YimJiYoung <chiy323@gmail.com>
2023-01-11 16:19:27 +0900

    refactor: change ratio type to number in SkeletonImage

--- packages/vibrant-components/src/lib/Skeleton/SkeletonImage/SkeletonImageProps.ts
+++ packages/vibrant-components/src/lib/Skeleton/SkeletonImage/SkeletonImageProps.ts
@@ -1,27 +1,9 @@
 import type { BorderSystemProps, ResponsiveValue } from '@vibrant-ui/core';
-import { propVariant, withVariation } from '@vibrant-ui/core';
+import { withVariation } from '@vibrant-ui/core';
 
 export type SkeletonImageProps = Pick<BorderSystemProps, 'borderRadiusLevel'> & {
   width: ResponsiveValue<number | '100%'>;
-  ratio: ResponsiveValue<'1:1' | '3:4' | '4:3' | '16:9'>;
+  ratio: ResponsiveValue<number>;
 };
 
-export const withSkeletonImageVariation = withVariation<SkeletonImageProps>('SkeletonImage')(
-  propVariant({
-    props: [{ name: 'ratio', responsive: true }],
-    variants: {
-      '1:1': {
-        ratio: 1,
-      },
-      '3:4': {
-        ratio: 3 / 4,
-      },
-      '4:3': {
-        ratio: 4 / 3,
-      },
-      '16:9': {
-        ratio: 16 / 9,
-      },
-    },
-  })
-);
+export const withSkeletonImageVariation = withVariation<SkeletonImageProps>('SkeletonImage')();
