e75a5df6626dcac4695ea718437895c52009f329
YimJiYoung <chiy323@gmail.com>
2023-01-16 16:45:23 +0900

    feat: add TableStringFilter component

---
+++ packages/vibrant-components/src/lib/TableFilter/TableStringFilter/TableStringFilterProps.ts
@@ -0,0 +1,15 @@
+import { withVariation } from '@vibrant-ui/core';
+import type { StringFilterOperator } from '../type';
+
+export type TableStringFilterProps = {
+  dataKey: string;
+  label: string;
+  placeholder?: string;
+  operators: StringFilterOperator[];
+  defaultValue?: {
+    value: string;
+    operator: StringFilterOperator;
+  };
+};
+
+export const withTableStringFilterVariation = withVariation<TableStringFilterProps>('TableStringFilter')();
