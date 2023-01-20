c9e2fa8ad3464ec84c0fffad9585ece01ac8081a
phillip-101 <phillip.kim@101.inc>
2023-01-20 11:05:46 +0900

    feat: define type of CardNumberField

--- packages/vibrant-components/src/lib/CardNumberField/CardNumberFieldProps.ts
+++ packages/vibrant-components/src/lib/CardNumberField/CardNumberFieldProps.ts
@@ -1,9 +1,20 @@
+import type { ReactElement } from 'react';
+import type { ReactElementChild } from '@vibrant-ui/core';
 import { withVariation } from '@vibrant-ui/core';
-import type { TextFieldProps } from '../TextField';
+import type { BaseInputProps } from '../../types';
 
-type BackdropProps = Omit<
-  TextFieldProps,
-  'autoCapitalize' | 'autoComplete' | 'prefix' | 'renderEnd' | 'suffix' | 'type'
->;
+type CardNumberFieldProps = Omit<BaseInputProps<string>, 'onValueChange'> & {
+  id?: string;
+  state?: 'default' | 'error';
+  label?: ReactElement | string;
+  placeholder?: ReactElement | string;
+  helperText?: ReactElement | string;
+  separator?: string;
+  clearable?: boolean;
+  readOnly?: boolean;
+  onFocus?: () => void;
+  onBlur?: () => void;
+  renderStart?: ReactElementChild;
+};
 
-export const withCardNumberFieldVariation = withVariation<BackdropProps>('Backdrop')();
+export const withCardNumberFieldVariation = withVariation<CardNumberFieldProps>('CardNumberField')();
