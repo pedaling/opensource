b881ddd59daa4f31761a2ae338dfb32bfc676ff2
phillip-101 <phillip.kim@101.inc>
2023-01-30 13:31:56 +0900

    feat: add CardImage assets folder

---
+++ packages/vibrant-components/src/lib/CardNumberField/assets/index.ts
@@ -0,0 +1,22 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+import * as PaymentMethodImageAmex from './PaymentMethodImageAmex.svg';
+import * as PaymentMethodImageDinersClub from './PaymentMethodImageDinersClub.svg';
+import * as PaymentMethodImageDiscover from './PaymentMethodImageDiscover.svg';
+import * as PaymentMethodImageJCB from './PaymentMethodImageJCB.svg';
+import * as PaymentMethodImageMaestro from './PaymentMethodImageMaestro.svg';
+import * as PaymentMethodImageMasterCard from './PaymentMethodImageMasterCard.svg';
+import * as paymentMethodImageOthers from './PaymentMethodImageOthers.svg';
+import * as PaymentMethodImageUnionPay from './PaymentMethodImageUnionPay.svg';
+import * as PaymentMethodImageVisa from './PaymentMethodImageVisa.svg';
+
+export default {
+  paymentMethodImageOthers,
+  PaymentMethodImageAmex,
+  PaymentMethodImageDinersClub,
+  PaymentMethodImageDiscover,
+  PaymentMethodImageJCB,
+  PaymentMethodImageMaestro,
+  PaymentMethodImageMasterCard,
+  PaymentMethodImageUnionPay,
+  PaymentMethodImageVisa,
+};
