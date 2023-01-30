3afe96b299a8dddec96fe5402412f90b920beefe
Mia(임지영) <37496919+YimJiYoung@users.noreply.github.com>
2023-01-30 09:41:43 +0900

    fix: change Skeleton background color to disable (#539)

--- packages/vibrant-components/src/lib/Skeleton/SkeletonChip/SkeletonChip.tsx
+++ packages/vibrant-components/src/lib/Skeleton/SkeletonChip/SkeletonChip.tsx
@@ -4,6 +4,6 @@ import { withSkeletonChipVariation } from './SkeletonChipProps';
 
 export const SkeletonChip = withSkeletonChipVariation(({ width = '100%', height }) => (
   <SkeletonMotion>
-    <Box backgroundColor="surface1" borderRadiusLevel={5} width={width} height={height}></Box>
+    <Box backgroundColor="disable" borderRadiusLevel={5} width={width} height={height}></Box>
   </SkeletonMotion>
 ));
