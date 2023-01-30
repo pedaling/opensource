d9e5e13c3c18f5f9f62d1fbcc4af61f95ba81bad
phillip-101 <phillip.kim@101.inc>
2023-01-30 15:23:25 +0900

    feat: appear cardImage according to input num, clearable

--- packages/vibrant-components/src/lib/CardNumberField/CardNumberField.tsx
+++ packages/vibrant-components/src/lib/CardNumberField/CardNumberField.tsx
@@ -1,142 +1,135 @@
-/* eslint-disable prettier/prettier */
-import validate from "card-validator";
-import { useState } from "react";
-import { Icon } from "@vibrant-ui/icons";
-import { MountMotion } from "@vibrant-ui/motion";
-import { Body } from "../Body";
-import { HStack } from "../HStack";
-import { Paper } from "../Paper";
-import { TextField } from "../TextField";
-import { VStack } from "../VStack";
-import { withCardNumberFieldVariation } from "./CardNumberFieldProps";
+import validate from 'card-validator';
+import { useState } from 'react';
+import { Image } from '@vibrant-ui/core';
+import { Icon } from '@vibrant-ui/icons';
+import { MountMotion } from '@vibrant-ui/motion';
+import { HStack } from '../HStack';
+import { TextField } from '../TextField';
+import { VStack } from '../VStack';
+import cardImage from './assets';
+import { withCardNumberFieldVariation } from './CardNumberFieldProps';
 
+type CardType =
+  | 'american-express'
+  | 'diners-club'
+  | 'discover'
+  | 'jcb'
+  | 'maestro'
+  | 'mastercard'
+  | 'others'
+  | 'unionpay'
+  | 'visa';
+type ImageType =
+  | 'PaymentMethodImageAmex'
+  | 'PaymentMethodImageDinersClub'
+  | 'PaymentMethodImageDiscover'
+  | 'PaymentMethodImageJCB'
+  | 'PaymentMethodImageMaestro'
+  | 'PaymentMethodImageMasterCard'
+  | 'PaymentMethodImageOthers'
+  | 'PaymentMethodImageUnionPay'
+  | 'PaymentMethodImageVisa';
+
+const CARD_TYPE_IMG_MAP: Record<CardType, ImageType> = {
+  'american-express': 'PaymentMethodImageAmex',
+  'diners-club': 'PaymentMethodImageDinersClub',
+  discover: 'PaymentMethodImageDiscover',
+  jcb: 'PaymentMethodImageJCB',
+  maestro: 'PaymentMethodImageMaestro',
+  mastercard: 'PaymentMethodImageMasterCard',
+  unionpay: 'PaymentMethodImageUnionPay',
+  visa: 'PaymentMethodImageVisa',
+  others: 'PaymentMethodImageOthers',
+};
 const DEFAULT_GAPS = [4, 8, 12];
 
-export const CardNumberField = withCardNumberFieldVariation(
-  ({ onValueChange, separator = " " }) => {
-    const [showLockIcon, setShowLockIcon] = useState(false);
+export const CardNumberField = withCardNumberFieldVariation(({ onValueChange, separator = ' ' }) => {
+  const [showLockIcon, setShowLockIcon] = useState(false);
 
-    const [value, setValue] = useState("");
+  const [value, setValue] = useState('');
 
-    const { card } = validate.number(value);
-    const cardType = card?.niceType ?? "others";
-    const gaps = card?.gaps ?? DEFAULT_GAPS;
+  const { card } = validate.number(value);
+  const cardType = (card?.type ?? 'others') as CardType;
+  const cardImageSource = cardImage[CARD_TYPE_IMG_MAP[cardType]]?.default;
+  const gaps = card?.gaps ?? DEFAULT_GAPS;
 
-    const prettyCardNumber = (cardNumber: string) => {
-      const offsets = [0].concat(gaps, cardNumber.length);
-      const components = [];
+  const prettyCardNumber = (cardNumber: string) => {
+    const offsets = [0].concat(gaps, cardNumber.length);
+    const components = [];
 
-      for (let i = 0; offsets[i] < cardNumber.length; i++) {
-        const start = offsets[i];
-        const end = Math.min(offsets[i + 1], cardNumber.length);
+    for (let i = 0; offsets[i] < cardNumber.length; i++) {
+      const start = offsets[i];
+      const end = Math.min(offsets[i + 1], cardNumber.length);
 
-        components.push(cardNumber.substring(start, end));
-      }
+      components.push(cardNumber.substring(start, end));
+    }
 
-      return components.join(separator);
+    return components.join(separator);
+  };
 
-      return cardNumber;
-    };
+  /**
+   * maxLength는 각 카드사의 포멧의 최대 길이에 맞춰 강제한다.
+   */
+  const maxLength = (() => {
+    if (card?.lengths) {
+      return Math.max(...card.lengths);
+    } else {
+      return 16;
+    }
+  })();
 
-    /**
-     * maxLength는 각 카드사의 포멧의 최대 길이에 맞춰 강제한다.
-     */
-    const maxLength = (() => {
-      if (card?.lengths) {
-        return Math.max(...card.lengths);
-      } else {
-        return 16;
-      }
-    })();
+  const mountAnimation = {
+    opacity: {
+      from: 0,
+      to: 1,
+    },
+  };
+  const unmountAnimation = {
+    opacity: {
+      from: 1,
+      to: 0,
+    },
+  };
 
-    const mountAnimation = {
-      opacity: {
-        from: 0,
-        to: 1,
-      },
-    };
-    const unmountAnimation = {
-      opacity: {
-        from: 1,
-        to: 0,
-      },
-    };
+  return (
+    <VStack minWidth={400}>
+      <TextField
+        type="number"
+        autoComplete="ccNumber"
+        clearable={true}
+        defaultValue={prettyCardNumber(value)}
+        renderEnd={() => (
+          <HStack alignVertical="center" spacing={12}>
+            <VStack>
+              <MountMotion
+                mountAnimation={mountAnimation}
+                unmountAnimation={unmountAnimation}
+                mount={value.length !== 0}
+                easing="easeOutQuad"
+                duration={200}
+              >
+                <Image width={22.38} src={cardImageSource} />
+              </MountMotion>
+            </VStack>
+            {showLockIcon && <Icon.Lock.Thin fill="onView2" />}
+          </HStack>
+        )}
+        maxLength={maxLength}
+        onValueChange={({ value, prevent }) => {
+          setValue(value);
 
-    return (
-      <VStack>
-        {/* <VStack>{card?.type}</VStack>
-        <VStack>{card?.code.name}</VStack>
-        <VStack>{card?.code.size}</VStack>
-        <VStack>{card?.lengths}</VStack>
-        <VStack>{card?.niceType}</VStack> */}
-        {/* <VStack>
-          {card?.gaps.map((v, idx) => (
-            <VStack key={idx}>{v}</VStack>
-          ))}
-        </VStack>
-        {/* <VStack>inputref{cardNumber}</VStack> */}
-        {/* 카드 이미지 고려하여 minWidth 설정 */}
-        <VStack minWidth={400}>
-          <TextField
-            type="number"
-            autoComplete="ccNumber"
-            defaultValue={prettyCardNumber(value)}
-            renderEnd={() => (
-              <HStack alignVertical="center" spacing={12}>
-                <VStack>
-                  <MountMotion
-                    mountAnimation={mountAnimation}
-                    unmountAnimation={unmountAnimation}
-                    mount={value.length !== 0}
-                    easing="easeOutQuad"
-                    duration={200}
-                  >
-                    <Paper width={50} backgroundColor="primary">
-                      <Body level={1}>{cardType}</Body>
-                    </Paper>
-                  </MountMotion>
-                </VStack>
-                {showLockIcon && <Icon.Lock.Thin fill="onView2" />}
-              </HStack>
-            )}
-            maxLength={maxLength}
-            onValueChange={({ value, prevent }) => {
-              setValue(value);
+          prevent();
+          if (value.length > 0) {
+            setShowLockIcon(true);
+          } else {
+            setShowLockIcon(false);
+          }
 
-              prevent();
-              if (value.length > 0) {
-                setShowLockIcon(true);
-              } else {
-                setShowLockIcon(false);
-              }
-
-              if (onValueChange) {
-                onValueChange({ value, prevent });
-              }
-            }}
-          />
-        </VStack>
-      </VStack>
-    );
-  }
-);
-
-{
-  /* <VStack>
-  <VStack>{card?.type}</VStack>
-  <VStack>{card?.code.name}</VStack>
-  <VStack>{card?.code.size}</VStack>
-  <VStack>{card?.lengths}</VStack>
-  <VStack>{card?.niceType}</VStack>
-  <VStack>
-    {card?.gaps.map((v, idx) => (
-      <VStack key={idx}>{v}</VStack>
-    ))}
-  </VStack>
-  <VStack>{`fasdf,${isValid}`}</VStack>
-  <VStack>
-    {card?.maxLength}
-    maxLength
-  </VStack>
-</VStack>; */
-}
+          if (onValueChange) {
+            onValueChange({ value, prevent });
+          }
+        }}
+      />
+    </VStack>
+  );
+});
--- packages/vibrant-components/src/lib/CardNumberField/assets/index.ts
+++ packages/vibrant-components/src/lib/CardNumberField/assets/index.ts
@@ -5,12 +5,12 @@ import * as PaymentMethodImageDiscover from './PaymentMethodImageDiscover.svg';
 import * as PaymentMethodImageJCB from './PaymentMethodImageJCB.svg';
 import * as PaymentMethodImageMaestro from './PaymentMethodImageMaestro.svg';
 import * as PaymentMethodImageMasterCard from './PaymentMethodImageMasterCard.svg';
-import * as paymentMethodImageOthers from './PaymentMethodImageOthers.svg';
+import * as PaymentMethodImageOthers from './PaymentMethodImageOthers.svg';
 import * as PaymentMethodImageUnionPay from './PaymentMethodImageUnionPay.svg';
 import * as PaymentMethodImageVisa from './PaymentMethodImageVisa.svg';
 
 export default {
-  paymentMethodImageOthers,
+  PaymentMethodImageOthers,
   PaymentMethodImageAmex,
   PaymentMethodImageDinersClub,
   PaymentMethodImageDiscover,
