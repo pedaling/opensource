f81ff591b76dfb85616a758b6336791fdbf0ba04
lou-lee <lou@101.inc>
2023-02-10 14:44:01 +0900

    fix: calculate responsive value in ContentArea

--- packages/vibrant-components/src/lib/ContentArea/ContentArea.tsx
+++ packages/vibrant-components/src/lib/ContentArea/ContentArea.tsx
@@ -1,15 +1,19 @@
-import { useCurrentTheme } from '@vibrant-ui/core';
+import { calculateResponsiveValues, useCurrentTheme } from '@vibrant-ui/core';
 import { VStack } from '../VStack';
 import { withContentAreaVariation } from './ContentAreaProps';
 
-export const ContentArea = withContentAreaVariation(({ padding = true, children }) => {
+export const ContentArea = withContentAreaVariation(({ padding, children }) => {
   const {
     theme: { contentArea },
   } = useCurrentTheme();
 
+  const { px } = calculateResponsiveValues({ padding, px: contentArea.padding }, value => ({
+    px: value.padding ? value.px : 0,
+  }));
+
   return (
     <VStack alignHorizontal="center" width="100%">
-      <VStack maxWidth={contentArea.maxWidth} width="100%" px={padding ? contentArea.padding : 0}>
+      <VStack maxWidth={contentArea.maxWidth} width="100%" px={px}>
         {children}
       </VStack>
     </VStack>
