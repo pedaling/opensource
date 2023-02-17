6869b3913e2adcf68da6a9eeac6a2169f44694b0
Hanna Jeong <jhn3981@gmail.com>
2023-02-17 16:55:57 +0900

    fix: add children to SkeletonProps

--- packages/vibrant-components/src/lib/Skeleton/Skeleton.stories.tsx
+++ packages/vibrant-components/src/lib/Skeleton/Skeleton.stories.tsx
@@ -1,4 +1,5 @@
 import type { ComponentMeta, ComponentStory } from '@storybook/react';
+import { VStack } from '../VStack';
 import { Skeleton } from './Skeleton';
 
 export default {
@@ -13,3 +14,11 @@ export default {
 } as ComponentMeta<typeof Skeleton>;
 
 export const Basic: ComponentStory<typeof Skeleton> = props => <Skeleton {...props} />;
+
+export const WithChildren: ComponentStory<typeof Skeleton> = props => (
+  <Skeleton {...props}>
+    <VStack p={10}>
+      <Skeleton.Text typography="body1" />
+    </VStack>
+  </Skeleton>
+);
--- packages/vibrant-components/src/lib/Skeleton/SkeletonProps.ts
+++ packages/vibrant-components/src/lib/Skeleton/SkeletonProps.ts
@@ -1,10 +1,11 @@
-import type { BorderSystemProps, ResponsiveValue } from '@vibrant-ui/core';
+import type { BorderSystemProps, ReactElementChild, ResponsiveValue } from '@vibrant-ui/core';
 import { withVariation } from '@vibrant-ui/core';
 
 export type SkeletonProps = Pick<BorderSystemProps, 'borderRadiusLevel'> & {
   width: ResponsiveValue<number | `${number}%`>;
   height: ResponsiveValue<number | `${number}%`>;
   maxWidth?: ResponsiveValue<number | `${number}%`>;
+  children?: ReactElementChild;
 };
 
 export const withSkeletonVariation = withVariation<SkeletonProps>('Skeleton')();
