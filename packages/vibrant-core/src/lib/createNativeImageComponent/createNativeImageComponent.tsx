8163d3abf6d821ff0fa6c6a4c8d562d2fc764b7d
Hanna Jeong <jhn3981@gmail.com>
2023-02-20 23:46:36 +0900

    fix: 리뷰반영

--- packages/vibrant-core/src/lib/createNativeImageComponent/createNativeImageComponent.tsx
+++ packages/vibrant-core/src/lib/createNativeImageComponent/createNativeImageComponent.tsx
@@ -3,14 +3,28 @@ import { forwardRef } from 'react';
 
 type ImageComponentType = ComponentType<{
   source: { uri: string };
+  resizeMode?: 'center' | 'contain' | 'cover' | 'stretch';
 }>;
 
 type ImageProps = {
   ref?: any;
   src: string;
+  objectFit?: 'contain' | 'cover' | 'fill' | 'none';
 };
 
+const ObjectFitResizeModeMap = {
+  contain: 'contain',
+  cover: 'cover',
+  fill: 'stretch',
+  none: 'center',
+} as const;
+
 export const createNativeImageComponent = (ImageComponent: ImageComponentType) =>
-  forwardRef<any, ImageProps>(({ src, ...restProps }, ref) => (
-    <ImageComponent ref={ref} source={{ uri: src }} {...restProps} />
+  forwardRef<any, ImageProps>(({ src, objectFit, ...restProps }, ref) => (
+    <ImageComponent
+      ref={ref}
+      source={{ uri: src }}
+      resizeMode={objectFit ? ObjectFitResizeModeMap[objectFit] : undefined}
+      {...restProps}
+    />
   ));
--- packages/vibrant-core/src/lib/props/media/media.native.ts
+++ packages/vibrant-core/src/lib/props/media/media.native.ts
@@ -2,11 +2,7 @@ import { createSystemProp } from '../../createSystemProp';
 
 const objectFitProp = createSystemProp({
   property: 'objectFit',
-  transform: value => ({
-    props: {
-      resizeMode: value === 'fill' ? 'stretch' : value,
-    },
-  }),
+  disabled: true,
 });
 
 export const mediaSystemProps = [objectFitProp];
