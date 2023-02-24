a1b80fd8a8405c66c0b56484c2549a37625ef4da
lou-lee <lou@101.inc>
2023-02-24 13:09:09 +0900

    feat: add nl2br utils into core and applies to Text component

--- packages/vibrant-core/src/lib/Text/Text.native.tsx
+++ packages/vibrant-core/src/lib/Text/Text.native.tsx
@@ -2,14 +2,23 @@ import type { FC } from 'react';
 import { forwardRef } from 'react';
 import { Text as RNText, StyleSheet } from 'react-native';
 import styled from '@emotion/native';
+import { nl2br } from '../nl2br';
 import type { TextProps } from './TextProps';
 import { interpolation, shouldForwardProp } from './TextProps';
 
 export const Text: FC<TextProps> = styled(
-  forwardRef<unknown, TextProps & { style?: any }>(({ style, ...restProps }, ref) => {
+  forwardRef<unknown, TextProps & { style?: any }>(({ style, children, ...restProps }, ref) => {
     const { props, ...restStyle } = StyleSheet.flatten(style);
 
-    return <RNText ref={ref} style={restStyle} {...restProps} {...props} />;
+    return (
+      <RNText
+        ref={ref}
+        style={restStyle}
+        children={typeof children === 'string' ? nl2br(children) : children}
+        {...restProps}
+        {...props}
+      />
+    );
   }),
   {
     shouldForwardProp,
