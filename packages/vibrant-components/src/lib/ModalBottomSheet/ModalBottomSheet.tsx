ac9246ab26690841ea2665184758b0674b3f32c6
Mia(임지영) <37496919+YimJiYoung@users.noreply.github.com>
2023-03-08 17:59:25 +0900

    fix: set button loading state in ModalBottomSheet (#609)

--- packages/vibrant-components/src/lib/ModalBottomSheet/ModalBottomSheet.tsx
+++ packages/vibrant-components/src/lib/ModalBottomSheet/ModalBottomSheet.tsx
@@ -185,6 +185,7 @@ export const ModalBottomSheet = withModalBottomSheetVariation(
                       onClick={() => primaryButtonOptions.onClick?.({ close: closeModal })}
                       full={true}
                       disabled={primaryButtonOptions.disabled}
+                      loading={primaryButtonOptions.loading}
                     >
                       {primaryButtonOptions.text}
                     </ContainedButton>
@@ -198,6 +199,7 @@ export const ModalBottomSheet = withModalBottomSheetVariation(
                       onClick={() => secondaryButtonOptions.onClick?.({ close: closeModal })}
                       full={true}
                       disabled={secondaryButtonOptions.disabled}
+                      loading={secondaryButtonOptions.loading}
                     >
                       {secondaryButtonOptions.text}
                     </ContainedButton>
@@ -207,6 +209,7 @@ export const ModalBottomSheet = withModalBottomSheetVariation(
                       onClick={() => primaryButtonOptions.onClick?.({ close: closeModal })}
                       full={true}
                       disabled={primaryButtonOptions.disabled}
+                      loading={primaryButtonOptions.loading}
                     >
                       {primaryButtonOptions.text}
                     </ContainedButton>
@@ -220,6 +223,7 @@ export const ModalBottomSheet = withModalBottomSheetVariation(
                       onClick={() => primaryButtonOptions.onClick?.({ close: closeModal })}
                       full={true}
                       disabled={primaryButtonOptions.disabled}
+                      loading={primaryButtonOptions.loading}
                     >
                       {primaryButtonOptions.text}
                     </ContainedButton>
