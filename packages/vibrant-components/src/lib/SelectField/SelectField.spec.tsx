bb4d1c0ada346faf23f5bea5b863d410eea311b7
YimJiYoung <chiy323@gmail.com>
2023-03-13 16:15:43 +0900

    test: add SelectField snapshot test

---
+++ packages/vibrant-components/src/lib/SelectField/SelectField.spec.tsx
@@ -0,0 +1,81 @@
+import type { ReactRenderer } from '@vibrant-ui/utils/testing-web';
+import { createReactRenderer } from '@vibrant-ui/utils/testing-web';
+import { SelectField } from './SelectField';
+
+describe('<SelectField />', () => {
+  const { render } = createReactRenderer();
+
+  let renderer: ReactRenderer;
+
+  describe('when size is lg', () => {
+    beforeEach(async () => {
+      renderer = render(
+        <SelectField
+          label="label"
+          options={[
+            {
+              label: 'option 1',
+              value: '1',
+            },
+            {
+              label: 'option 2',
+              value: '2',
+            },
+          ]}
+        />
+      );
+    });
+
+    it('match snapshot', () => {
+      expect(renderer.container).toMatchSnapshot();
+    });
+  });
+
+  describe('when size is md', () => {
+    beforeEach(async () => {
+      renderer = render(
+        <SelectField
+          label="label"
+          options={[
+            {
+              label: 'option 1',
+              value: '1',
+            },
+            {
+              label: 'option 2',
+              value: '2',
+            },
+          ]}
+        />
+      );
+    });
+
+    it('match snapshot', () => {
+      expect(renderer.container).toMatchSnapshot();
+    });
+  });
+
+  describe('when size is sm', () => {
+    beforeEach(async () => {
+      renderer = render(
+        <SelectField
+          label="label"
+          options={[
+            {
+              label: 'option 1',
+              value: '1',
+            },
+            {
+              label: 'option 2',
+              value: '2',
+            },
+          ]}
+        />
+      );
+    });
+
+    it('match snapshot', () => {
+      expect(renderer.container).toMatchSnapshot();
+    });
+  });
+});
