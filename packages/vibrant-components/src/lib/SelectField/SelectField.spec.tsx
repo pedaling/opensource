22e9ad3d8720a17344ef65d7818f0fd0534af191
YimJiYoung <chiy323@gmail.com>
2023-03-14 17:15:01 +0900

    test: add test case when defaultValue changed onValueChange should be called

--- packages/vibrant-components/src/lib/SelectField/SelectField.spec.tsx
+++ packages/vibrant-components/src/lib/SelectField/SelectField.spec.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import type { ReactRenderer } from '@vibrant-ui/utils/testing-web';
 import { createReactRenderer } from '@vibrant-ui/utils/testing-web';
 import { SelectField } from './SelectField';
@@ -6,6 +7,46 @@ describe('<SelectField />', () => {
   const { render } = createReactRenderer();
 
   let renderer: ReactRenderer;
+  let mockOnValueChange: jest.Mock<any, any>;
+
+  describe('when defaultValue changed', () => {
+    beforeEach(async () => {
+      mockOnValueChange = jest.fn();
+
+      // eslint-disable-next-line @typescript-eslint/naming-convention
+      const ControlledSelectField = () => {
+        const [defaultValue, setDefaultValue] = useState('1');
+
+        useEffect(() => {
+          setDefaultValue('2');
+        }, []);
+
+        return (
+          <SelectField
+            label="label"
+            defaultValue={defaultValue}
+            onValueChange={mockOnValueChange}
+            options={[
+              {
+                label: 'option 1',
+                value: '1',
+              },
+              {
+                label: 'option 2',
+                value: '2',
+              },
+            ]}
+          />
+        );
+      };
+
+      renderer = render(<ControlledSelectField />);
+    });
+
+    it('onValueChange should be called', () => {
+      expect(mockOnValueChange).toBeCalledWith('2');
+    });
+  });
 
   describe('when size is lg', () => {
     beforeEach(async () => {
