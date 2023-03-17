835cf76e7b70bcd1ccdfd1982489b1b3b917979d
lou-lee <lou@101.inc>
2023-03-17 18:39:33 +0900

    feat: update layout

---
+++ packages/vibrant-website/src/components/CoreValueCard.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Body, Display, HStack, Paper, Title, VStack } from '@vibrant-ui/components';
+
+type CoreValueCardProps = {
+  coreValueEmoji: string;
+  title: string;
+  content: string;
+};
+
+export const CoreValueCard = ({ content, coreValueEmoji, title }: CoreValueCardProps) => (
+  <VStack spacing={32} alignHorizontal="center" p={24}>
+    <Paper width={150} height={150} borderRadiusLevel={4} backgroundColor="informativeContainer">
+      <HStack height="100%" alignHorizontal="center" alignVertical="center">
+        <Display level={3}>{coreValueEmoji}</Display>
+      </HStack>
+    </Paper>
+
+    <VStack alignHorizontal="center" spacing={12}>
+      <Title as="h3" level={5} color="informative">
+        {title}
+      </Title>
+      <Body as="p" level={[1, 2, 2]} color="onView2" textAlign="center" wordBreak="keep-all">
+        {content}
+      </Body>
+    </VStack>
+  </VStack>
+);
--- packages/vibrant-website/src/pages/index.tsx
+++ packages/vibrant-website/src/pages/index.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import Translate, { translate } from '@docusaurus/Translate';
+import { translate } from '@docusaurus/Translate';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
-import { Body, Display, HStack, Title, VStack } from '@vibrant-ui/components';
+import { Display, Paragraph, Stack, Title, VStack } from '@vibrant-ui/components';
 import { Image } from '@vibrant-ui/core';
+import { CoreValueCard } from '../components/CoreValueCard';
 
 const Home = (): JSX.Element => {
   const { siteConfig } = useDocusaurusContext();
@@ -18,7 +19,11 @@ const Home = (): JSX.Element => {
       })}
     >
       <VStack width="100%">
-        <Image src="/img/graphic_background.png" />
+        <Image
+          maxHeight={720}
+          objectFit="cover"
+          src="https://cdn.class101.net/attachment/ca5fbcf0-4b02-4034-99d1-1a8b35132c5e.png"
+        />
 
         <VStack
           width="100%"
@@ -31,57 +36,46 @@ const Home = (): JSX.Element => {
           <Display as="h1" level={[4, 4, 3]} color="white">
             Vibrant Design System
           </Display>
-          <Body as="p" color="white" level={[4, 4, 2]} textAlign="center">
+          <Paragraph as="p" color="white" weight="regular" level={3} textAlign="center">
             Class101의 사용자들이 보다 일관적인 서비스를 경험하기 위하여\n효율적이고 우수한 성능의 프로덕트를 제작할 수
             있도록 고안된 시스템입니다.
-          </Body>
+          </Paragraph>
         </VStack>
       </VStack>
 
-      <VStack px={32} py={64} spacing={24}>
-        <Title as="h2" level={4}>
+      <VStack width="100%" px={32} pt={80} pb={64} spacing={32} alignHorizontal="center">
+        <Title as="h2" level={3} weight="extraBold">
           Core Value
         </Title>
 
-        <HStack spacing={100}>
-          <VStack spacing={12}>
-            <Image src="/img/graphic_background_2.png" />
-            <Title as="h3" level={6}>
-              Performant
-            </Title>
-            <Body as="p" level={1} color="onView2">
-              <Translate id="homepage.core-value.performant">
-                효과적으로 제작된 Class101의 로덕트는 최고의 성능으로 기능하여 유저에게 긍정적인 사용자 경험을 줄 수
-                있습니다.
-              </Translate>
-            </Body>
-          </VStack>
-          <VStack spacing={12}>
-            <Image src="/img/graphic_background_2.png" />
-            <VStack p={20}>
-              <Title as="h3" level={6}>
-                Productive
-              </Title>
-              <Body as="p" level={1} color="onView2">
-                <Translate id="homepage.core-value.productive">
-                  Class101의 빌더들이 보다 생산적으로 일할 수 있도록 하여 보다 적은 노력으로 같은 조건의 결과물을 산출할
-                  수 있습니다.
-                </Translate>
-              </Body>
-            </VStack>
-          </VStack>
-          <VStack spacing={12}>
-            <Image src="/img/graphic_background_2.png" />
-            <Title as="h3" level={6}>
-              Consistent
-            </Title>
-            <Body as="p" level={1} color="onView2">
-              <Translate id="homepage.core-value.consistent">
-                사용자들이 플랫폼에 관계 없이 일관된 프로덕트를 경험할 수 있는 일관적인 사용자 경험을 줄 수 있습니다.
-              </Translate>
-            </Body>
-          </VStack>
-        </HStack>
+        <Stack
+          direction={['vertical', 'vertical', 'horizontal']}
+          width="100%"
+          height="100%"
+          alignVertical="center"
+          alignHorizontal="center"
+          spacing={40}
+        >
+          <CoreValueCard
+            content="효과적으로 제작된 Class101의 프로덕트는 최고의 성능으로 기능하여\n유저에게 긍정적인 사용자 경험을 줄 수 있습니다."
+            coreValueEmoji="🚀"
+            title="Performant"
+            key="Performant"
+          />
+          <CoreValueCard
+            content="Class101의 빌더들이 보다 생산적으로 일할 수 있도록 하여\n보다 적은 노력으로 같은 조건의 결과물을 산출할 수 있습니다."
+            coreValueEmoji="⚡"
+            title="Productive"
+            key="Productive"
+          />
+
+          <CoreValueCard
+            content="사용자들이 플랫폼에 관계 없이 일관된 프로덕트를 경험할 수 있는\n일관적인 사용자 경험을 줄 수 있습니다."
+            coreValueEmoji="🏰"
+            title="Consistent"
+            key="consistent"
+          />
+        </Stack>
       </VStack>
     </Layout>
   );
