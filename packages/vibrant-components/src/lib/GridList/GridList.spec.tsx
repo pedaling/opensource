35f7230b5d7a475477c9f72341f0777acce89eb7
MIA <mia@MIA.local>
2023-03-24 11:37:07 +0900

    test: fill test case

--- packages/vibrant-components/src/lib/GridList/GridList.spec.tsx
+++ packages/vibrant-components/src/lib/GridList/GridList.spec.tsx
@@ -1,44 +1,90 @@
+import { fireEvent, waitFor } from '@testing-library/react';
+import { Box } from '@vibrant-ui/core';
 import type { ReactRenderer } from '@vibrant-ui/utils/testing-web';
 import { createReactRenderer } from '@vibrant-ui/utils/testing-web';
+import { Body } from '../Body';
+import { GridList } from './GridList';
 
 describe('<GridList />', () => {
   const { render } = createReactRenderer();
 
   let renderer: ReactRenderer;
+  let element: HTMLElement;
 
-  describe('when columns set', () => {
-    // beforeEach(async () => {
-    //   renderer = render(<NotificationBadge data-testid="notificationBadge" kind="number" count={5} />);
-    //   element = await renderer.getByTestId('notificationBadge');
-    // });
-    // it('count rendered', () => {
-    //   expect(element.children[0].tagName).toBe('SPAN');
-    //   expect(element.children[0].textContent).toBe('5');
-    // });
-    it('the number of columns should be columns', () => {});
+  describe("when the 'columns' prop provided", () => {
+    beforeEach(async () => {
+      renderer = render(
+        <GridList
+          data={[1, 2, 3, 4, 5]}
+          columns={2}
+          renderItem={({ item }) => (
+            <Body level={1} color="onPrimary">
+              {item}
+            </Body>
+          )}
+          keyExtractor={item => item.toString()}
+        />
+      );
+
+      element = await renderer.getByTestId('grid-list');
+    });
+
+    it('the number of columns should be columns', () => {
+      expect(element).toHaveStyleRule('grid-template-columns', 'repeat(2, 1fr)');
+    });
   });
 
-  describe('when maxRows set', () => {
-    // beforeEach(async () => {
-    //   renderer = render(<NotificationBadge data-testid="notificationBadge" kind="number" count={5} />);
-    //   element = await renderer.getByTestId('notificationBadge');
-    // });
-    // it('count rendered', () => {
-    //   expect(element.children[0].tagName).toBe('SPAN');
-    //   expect(element.children[0].textContent).toBe('5');
-    // });
-    it('items in a row after maxRow should not be displayed', () => {});
+  describe("when the 'maxRows' prop provided", () => {
+    beforeEach(async () => {
+      renderer = render(
+        <GridList
+          data={[1, 2, 3, 4, 5]}
+          columns={2}
+          maxRows={2}
+          renderItem={({ item }) => (
+            <Body level={1} color="onPrimary">
+              {item}
+            </Body>
+          )}
+          keyExtractor={item => item.toString()}
+        />
+      );
+
+      element = await renderer.getByTestId('grid-list');
+    });
+
+    it('Any items exceeding maxRow should not be displayed.', () => {
+      expect(element.children[4]).toHaveStyleRule('display', 'none');
+    });
   });
 
-  describe('when onEndReached provided and scrolled to end item', () => {
-    it('onEndReached should be called', () => {});
-    // beforeEach(async () => {
-    //   renderer = render(<NotificationBadge data-testid="notificationBadge" kind="number" count={5} />);
-    //   element = await renderer.getByTestId('notificationBadge');
-    // });
-    // it('count rendered', () => {
-    //   expect(element.children[0].tagName).toBe('SPAN');
-    //   expect(element.children[0].textContent).toBe('5');
-    // });
+  describe("when the 'onEndReached' prop provided and the user has scrolled to the end of an item", () => {
+    const onEndReachedMockFn = jest.fn();
+
+    beforeEach(async () => {
+      renderer = render(
+        <GridList
+          data={[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]}
+          columns={1}
+          renderItem={({ item }) => (
+            <Box height={50}>
+              <Body level={1} color="onPrimary">
+                {item}
+              </Body>
+            </Box>
+          )}
+          keyExtractor={item => item.toString()}
+          onEndReached={onEndReachedMockFn}
+        />
+      );
+
+      element = await renderer.getByTestId('grid-list');
+
+      fireEvent.scroll(element, { target: { scrollY: 500 } });
+    });
+
+    it('onEndReached should be called', async () => {
+      await waitFor(() => expect(onEndReachedMockFn).toBeCalled());
+    });
   });
 });
