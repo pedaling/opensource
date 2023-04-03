ea99cc8eb9c8e2040b58d5031cb871756647ca1b
YimJiYoung <chiy323@gmail.com>
2023-04-03 17:15:42 +0900

    feat: use customization context avatar placeholder in Avatar

--- packages/vibrant-components/src/lib/Avatar/Avatar.stories.tsx
+++ packages/vibrant-components/src/lib/Avatar/Avatar.stories.tsx
@@ -6,8 +6,8 @@ export default {
   component: Avatar,
   args: {
     size: 'lg',
-    src: 'https://cdn.class101.net/images/7776e542-d6db-49c6-b86f-7e93f0cf5425/200x200.webp',
-    placeholder: 'https://cdn.class101.net/images/e1cba897-73d1-43de-864b-c36cefdea670/200x200.webp',
+    src: 'https://cdn.class101.net/images/7776e542-d6db-49c6-b86f-7e93f0cf5425/200x200.png',
+    placeholder: 'https://cdn.class101.net/images/e1cba897-73d1-43de-864b-c36cefdea670/200x200.png',
     alt: 'avatar',
   },
 } as ComponentMeta<typeof Avatar>;
--- packages/vibrant-components/src/lib/Avatar/Avatar.tsx
+++ packages/vibrant-components/src/lib/Avatar/Avatar.tsx
@@ -1,15 +1,25 @@
 import { useState } from 'react';
 import { Image } from '@vibrant-ui/core';
+import { useCustomization } from '../CustomizationProvider';
 import { withAvatarVariation } from './AvatarProps';
 
 export const Avatar = withAvatarVariation(({ testId, src, alt, size, placeholder }) => {
-  const [source, setSource] = useState(src);
+  const { avatar } = useCustomization();
+  const [fallbackSrc, setFallbackSrc] = useState<string>();
 
   const handleError = () => {
-    if (placeholder) setSource(placeholder);
+    setFallbackSrc(placeholder ?? avatar?.placeholder);
   };
 
   return (
-    <Image testId={testId} src={source} alt={alt} width={size} height={size} borderRadius="50%" onError={handleError} />
+    <Image
+      testId={testId}
+      src={fallbackSrc ?? src}
+      alt={alt}
+      width={size}
+      height={size}
+      borderRadius="50%"
+      onError={handleError}
+    />
   );
 });
