f342da2d99e0b1869a4cdf95ffbc2b7237262cb8
phillip-101 <phillip.kim@101.inc>
2023-04-04 14:57:34 +0900

    feat: add card icon map on customization provider

--- packages/vibrant-components/src/lib/CustomizationProvider/CustomizationProvider.tsx
+++ packages/vibrant-components/src/lib/CustomizationProvider/CustomizationProvider.tsx
@@ -1,10 +1,13 @@
-import type { FC } from 'react';
+import type { ComponentType, FC } from 'react';
 import { createContext, useContext, useMemo } from 'react';
-import type { ReactElementChild } from '@vibrant-ui/core';
+import type { ReactElementChild, ResponsiveValue } from '@vibrant-ui/core';
 
 export type Configurations = {
   avatar?: {
     placeholder: string;
+  };
+  cardNumberField?: {
+    cardIconMap: Record<string, ComponentType<{ size: ResponsiveValue<number> }>>;
   };
 };
 
@@ -13,6 +16,9 @@ type CustomizationContextValue = Configurations;
 const CustomizationContext = createContext<CustomizationContextValue>({
   avatar: {
     placeholder: 'https://cdn.class101.net/images/e1cba897-73d1-43de-864b-c36cefdea670/200x200.png',
+  },
+  cardNumberField: {
+    cardIconMap: {},
   },
 });
 
