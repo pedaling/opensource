a4447922bc2963dadf9957a37d56335c42e52e92
Jongminfire <jongminfire@gmail.com>
2023-04-06 11:51:21 +0900

    chore: breadcrumb 분기

--- packages/vibrant-components/src/lib/BreadCrumb/BreadCrumb.tsx
+++ packages/vibrant-components/src/lib/BreadCrumb/BreadCrumb.tsx
@@ -4,17 +4,19 @@ import { Pressable } from '../Pressable';
 import { withBreadCrumbVariation } from './BreadCrumbProps';
 
 export const BreadCrumb = withBreadCrumbVariation(({ color = 'onView2', children, href, isExternal, onClick }) => (
-  <Pressable onClick={onClick} cursor={href || onClick ? 'pointer' : 'default'}>
+  <>
     {href ? (
-      <Link href={href} isExternal={isExternal}>
+      <Link href={href} isExternal={isExternal} onClick={onClick}>
         <Body level={2} weight="regular" color={color} lineLimit={1}>
           {children}
         </Body>
       </Link>
     ) : (
-      <Body level={2} weight="regular" color={color} lineLimit={1}>
-        {children}
-      </Body>
+      <Pressable onClick={onClick} cursor={onClick ? 'pointer' : 'default'}>
+        <Body level={2} weight="regular" color={color} lineLimit={1}>
+          {children}
+        </Body>
+      </Pressable>
     )}
-  </Pressable>
+  </>
 ));
