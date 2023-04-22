36a20557908b65ec180ab93c6e2a95737a642cc9
Hanna Jeong <jhn3981@gmail.com>
2023-04-22 15:04:20 +0900

    test: add SelectField onValueChange test

--- packages/vibrant-components/src/lib/SelectField/SelectField.spec.tsx
+++ packages/vibrant-components/src/lib/SelectField/SelectField.spec.tsx
@@ -1,3 +1,6 @@
+import { waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { PortalRootProvider } from '@vibrant-ui/core';
 import type { ReactRenderer } from '@vibrant-ui/utils/testing-web';
 import { createReactRenderer } from '@vibrant-ui/utils/testing-web';
 import { SelectField } from './SelectField';
@@ -79,6 +82,44 @@ describe('<SelectField />', () => {
 
     it('match snapshot', () => {
       expect(renderer.container).toMatchSnapshot();
+    });
+  });
+
+  describe('when select option', () => {
+    const handleValueChange = jest.fn();
+
+    beforeEach(async () => {
+      renderer = render(
+        <PortalRootProvider zIndex={1}>
+          <SelectField
+            size="sm"
+            label="label"
+            onValueChange={handleValueChange}
+            options={[
+              {
+                label: 'option 1',
+                value: '1',
+              },
+              {
+                label: 'option 2',
+                value: '2',
+              },
+            ]}
+          />
+        </PortalRootProvider>
+      );
+
+      const selectField = await renderer.findByTestId('select-field');
+
+      await waitFor(() => userEvent.click(selectField.children[1]));
+
+      const optionItems = await renderer.findAllByTestId('select-option-item');
+
+      await waitFor(() => optionItems[0].click());
+    });
+
+    it('onValueChange called', () => {
+      expect(handleValueChange).toBeCalledTimes(1);
     });
   });
 });
