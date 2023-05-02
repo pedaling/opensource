2278aa862ecf649e471716f2b1235918266333b3
YimJiYoung <chiy323@gmail.com>
2023-05-02 13:31:35 +0900

    fix: handle responsiveValue and easing

--- packages/vibrant-motion/src/lib/Transition/Transition.tsx
+++ packages/vibrant-motion/src/lib/Transition/Transition.tsx
@@ -1,19 +1,32 @@
 import { cloneElement, useEffect, useMemo, useRef, useState } from 'react';
-import { useInterpolation } from '@vibrant-ui/core';
+import { useInterpolation, useResponsiveValue } from '@vibrant-ui/core';
 import { useCallbackRef, useComposedRef } from '@vibrant-ui/utils';
+import { timingFunctions } from '../constants/timingFunctions';
+import { transformMotionProps } from '../props/transform';
 import { withTransitionVariation } from './TransitionProp';
 
 export const Transition = withTransitionVariation(
   ({ innerRef, children, style, animation, duration = 200, easing = 'easeOutQuad', onEnd }) => {
-    const { interpolation } = useInterpolation();
+    const { interpolation } = useInterpolation(transformMotionProps);
 
     const elementRef = useRef<HTMLElement>(null);
     const composedRef = useComposedRef(innerRef, elementRef);
     const interpolationRef = useCallbackRef(interpolation);
-    const [animationStyle, setAnimationStyle] = useState(interpolation(animation));
+    const { getResponsiveValue } = useResponsiveValue();
+    const [animationStyle, setAnimationStyle] = useState(
+      interpolation(
+        Object.fromEntries(Object.entries(animation).map(([key, value]) => [key, getResponsiveValue(value)]))
+      )
+    );
 
     useEffect(() => {
-      requestAnimationFrame(() => setAnimationStyle(interpolationRef(animation)));
+      requestAnimationFrame(() =>
+        setAnimationStyle(
+          interpolationRef(
+            Object.fromEntries(Object.entries(animation).map(([key, value]) => [key, getResponsiveValue(value)]))
+          )
+        )
+      );
       // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [JSON.stringify(animation), interpolationRef]);
 
@@ -27,6 +40,7 @@ export const Transition = withTransitionVariation(
         ...animationStyle,
         transitionProperty: properties,
         transitionDuration: `${duration}ms`,
+        transitionTimingFunction: `cubic-bezier(${timingFunctions[easing].join(',')})`,
       },
     });
   }
