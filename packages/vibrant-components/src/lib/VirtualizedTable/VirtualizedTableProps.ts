b8c0694898b0cecfb13837fc5980221e63d76920
Maxi(맥시) <100735594+maxi-101@users.noreply.github.com>
2023-05-10 10:09:56 +0900

    fix: use table props for virtualized table props

--- packages/vibrant-components/src/lib/VirtualizedTable/VirtualizedTableProps.ts
+++ packages/vibrant-components/src/lib/VirtualizedTable/VirtualizedTableProps.ts
@@ -1,27 +1,5 @@
-/* eslint-disable @typescript-eslint/naming-convention */
-import type { ReactElement } from 'react';
-import type { ReactElementChild, TextChildren } from '@vibrant-ui/core';
-import type { TableColumnProps } from '../Table/TableColumn/TableColumnProps';
-import type { SortDirection } from '../Table/TableSortIcon';
+import type { TableProps } from '../Table/TableProps';
 
 export type VirtualizedTableProps<Data extends Record<string, any>, RowKey extends keyof Data> = {
-  data: Data[];
-  rowKey: RowKey;
-  loading?: boolean;
-  selectable?: boolean;
-  selectButtons?: { text: string; onClick: (selectedRows: Data[]) => void }[];
-  onSelectionChange?: (selectedRowKeys: Data[RowKey][]) => void;
-  renderExpanded?: (row: Data) => ReactElementChild;
-  children: (ReactElement<TableColumnProps<Data>> | ReactElement<TableColumnProps<Data>>[] | boolean | null)[];
-  onRow?: {
-    onClick: (row: Data) => void;
-  };
-  onSort?: ({ dataKey, direction }: { dataKey?: keyof Data; direction: SortDirection }) => void;
-  emptyText?: TextChildren;
-  emptyImage?: string;
-  disabledRowKeys?: Data[RowKey][];
-  expandedRowKeys?: Data[RowKey][];
-  tableLayout?: 'auto' | 'fixed';
-  testId?: string;
   height?: number;
-};
+} & TableProps<Data, RowKey>;
