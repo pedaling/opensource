f9c190bbbab0571a2f10f69d283c0b6f2cc505f1
Phillip <phillip.kim@101.inc>
2023-05-10 20:47:11 +0900

    refactor: use reduce, conditional operator, feat: set default value

--- packages/vibrant-components/src/lib/CardNumberField/CardNumberField.tsx
+++ packages/vibrant-components/src/lib/CardNumberField/CardNumberField.tsx
@@ -10,100 +10,97 @@ import { withCardNumberFieldVariation } from './CardNumberFieldProps';
 
 const DEFAULT_GAPS = [4, 8, 12];
 
-export const CardNumberField = withCardNumberFieldVariation(({ separator = '', onValueChange, ...restProps }) => {
-  const { cardNumberField } = useCustomization();
+export const CardNumberField = withCardNumberFieldVariation(
+  ({ separator = '', onValueChange, defaultValue, ...restProps }) => {
+    const { cardNumberField } = useCustomization();
 
-  const cardIconMap = cardNumberField?.cardIconMap;
+    const cardIconMap = cardNumberField?.cardIconMap;
 
-  const [showLockIcon, setShowLockIcon] = useState(false);
-  const [value, setValue] = useState('');
+    const [showLockIcon, setShowLockIcon] = useState(false);
+    const [value, setValue] = useState(defaultValue ?? '');
 
-  const { card } = validate.number(value);
-  const cardType = card?.type ?? 'others';
+    const { card } = validate.number(value);
+    const cardType = card?.type ?? 'others';
 
-  // eslint-disable-next-line @typescript-eslint/naming-convention
-  const CardIcon = cardIconMap?.[cardType] ?? null;
-  const gaps: number[] = card?.gaps ?? DEFAULT_GAPS;
+    // eslint-disable-next-line @typescript-eslint/naming-convention
+    const CardIcon = cardIconMap?.[cardType] ?? null;
+    const gaps: number[] = card?.gaps ?? DEFAULT_GAPS;
 
-  const onCardNumberChange = (newCardNumber: string) => {
-    const newCardNumberOnlyNumber = newCardNumber.match(/\d/g) ?? [];
-    const totalLength = newCardNumberOnlyNumber.length;
-    const numberGroups: string[] = [];
-    let startIndex = 0;
+    const onCardNumberChange = (newCardNumber: string) => {
+      const newCardNumberOnlyNumber = newCardNumber.match(/\d/g) ?? [];
+      const totalLength = newCardNumberOnlyNumber.length;
 
-    [...gaps, maxLength].forEach(breakPoint => {
-      const endIndex = Math.min(breakPoint, totalLength);
+      const a = [...gaps, maxLength];
+      const newCardNumberWithDash = a.reduce((acc, breakPoint, index) => {
+        const endIndex = Math.min(breakPoint, totalLength);
 
-      const group = newCardNumberOnlyNumber.slice(startIndex, endIndex).join('');
+        const group = newCardNumberOnlyNumber.slice(a[index - 1], endIndex).join('');
 
-      if (group) {
-        numberGroups.push(group);
-      }
+        if (group) {
+          if (acc) {
+            return acc + separator + group;
+          }
 
-      startIndex = endIndex;
-    });
-
-    const newCardNumberWithDash = numberGroups.join(separator);
-
-    setValue(newCardNumberWithDash);
-  };
-  const maxLength = (() => {
-    if (card?.lengths) {
-      return Math.max(...card.lengths) + gaps.length;
-    } else {
-      return 19;
-    }
-  })();
-
-  const mountAnimation = {
-    opacity: {
-      from: 0,
-      to: 1,
-    },
-  };
-  const unmountAnimation = {
-    opacity: {
-      from: 1,
-      to: 0,
-    },
-  };
-
-  return (
-    <TextField
-      {...restProps}
-      type="number"
-      defaultValue={value}
-      onValueChange={({ value, prevent }) => {
-        onValueChange?.({ value, prevent });
-
-        onCardNumberChange(value);
-        if (value.length > 0) {
-          setShowLockIcon(true);
-        } else {
-          setShowLockIcon(false);
+          return acc + group;
         }
 
-        prevent();
-      }}
-      maxLength={maxLength}
-      renderEnd={() => (
-        <HStack alignVertical="center" spacing={12}>
-          <VStack>
-            {CardIcon && (
-              <MountMotion
-                mountAnimation={mountAnimation}
-                unmountAnimation={unmountAnimation}
-                mount={value.length !== 0}
-                easing="easeOutQuad"
-                duration={200}
-              >
-                <CardIcon size={22.38} />
-              </MountMotion>
-            )}
-          </VStack>
-          {showLockIcon && <Icon.Lock.Thin fill="onView2" />}
-        </HStack>
-      )}
-    />
-  );
-});
+        return acc;
+      }, '');
+
+      setValue(newCardNumberWithDash);
+    };
+    const maxLength = card?.lengths ? Math.max(...card.lengths) + gaps.length : 19;
+
+    const mountAnimation = {
+      opacity: {
+        from: 0,
+        to: 1,
+      },
+    };
+    const unmountAnimation = {
+      opacity: {
+        from: 1,
+        to: 0,
+      },
+    };
+
+    return (
+      <TextField
+        {...restProps}
+        type="number"
+        defaultValue={value}
+        onValueChange={({ value, prevent }) => {
+          onValueChange?.({ value, prevent });
+
+          onCardNumberChange(value);
+          if (value.length > 0) {
+            setShowLockIcon(true);
+          } else {
+            setShowLockIcon(false);
+          }
+
+          prevent();
+        }}
+        maxLength={maxLength}
+        renderEnd={() => (
+          <HStack alignVertical="center" spacing={12}>
+            <VStack>
+              {CardIcon && (
+                <MountMotion
+                  mountAnimation={mountAnimation}
+                  unmountAnimation={unmountAnimation}
+                  mount={value.length !== 0}
+                  easing="easeOutQuad"
+                  duration={200}
+                >
+                  <CardIcon size={22.38} />
+                </MountMotion>
+              )}
+            </VStack>
+            {showLockIcon && <Icon.Lock.Thin fill="onView2" />}
+          </HStack>
+        )}
+      />
+    );
+  }
+);
