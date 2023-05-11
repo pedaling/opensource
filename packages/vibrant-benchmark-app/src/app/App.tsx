2728e451a82beb1b3fc171930a74f707a709e47b
Mia(임지영) <37496919+YimJiYoung@users.noreply.github.com>
2023-05-11 11:42:37 +0900

    feat: benchmark react-native 앱을 추가한다 (#711)
    * feat: add react-native vibrant-benchmark-app application
    
    * chore: remove e2e setting
    
    * fix: fit lint
    
    * chore: fix lint
    
    * chore: remove useless content
    
    * chore: remove jest related packages
    
    * chore: remove @nrwl/detox package
    
    * chore: update project.pbxproj
    
    * chore: remove vibrant-example-app from test coverage

---
+++ packages/vibrant-benchmark-app/src/app/App.tsx
@@ -0,0 +1,41 @@
+/* eslint-disable jsx-a11y/accessible-emoji */
+import React from 'react';
+import { SafeAreaView, ScrollView, StatusBar, StyleSheet, Text, View } from 'react-native';
+
+export const App = () => (
+  <>
+    <StatusBar barStyle="dark-content" />
+    <SafeAreaView>
+      <ScrollView contentInsetAdjustmentBehavior="automatic" style={styles.scrollView}>
+        <View style={styles.section}>
+          <Text style={styles.textLg}>Hello there,</Text>
+          <Text style={[styles.textXl, styles.appTitleText]} testID="heading">
+            Welcome VibrantBenchmarkApp 👋
+          </Text>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  </>
+);
+
+const styles = StyleSheet.create({
+  scrollView: {
+    backgroundColor: '#ffffff',
+  },
+  textLg: {
+    fontSize: 24,
+  },
+  textXl: {
+    fontSize: 48,
+  },
+  section: {
+    marginVertical: 12,
+    marginHorizontal: 12,
+  },
+  appTitleText: {
+    paddingTop: 12,
+    fontWeight: '500',
+  },
+});
+
+export default App;
