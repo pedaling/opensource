bbe2f1bfd1ba7527a1dac750fc1ee41e23ab12d0
lou-lee <lou@101.inc>
2023-05-11 14:34:47 +0900

    feat: add layout of benchmark application

---
+++ packages/vibrant-benchmark-app/src/app/VibrantApp.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Title, VStack } from '@vibrant-ui/components';
+
+export const VibrantApp = () => (
+  <VStack>
+    <Title level={3}>Vibrant App</Title>
+  </VStack>
+);
