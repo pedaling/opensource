a2ee5d466bed11df07e3c9629ad3c3f8a8757199
Atom(아톰) <100661305+atom-class@users.noreply.github.com>
2023-05-12 17:03:11 +0900

    refactor: update TableHeader component

--- packages/vibrant-components/src/lib/TableHeader/TableHeader.tsx
+++ packages/vibrant-components/src/lib/TableHeader/TableHeader.tsx
@@ -1,12 +1,37 @@
+import { Children } from 'react';
+import type { ReactElement } from 'react';
 import { HStack } from '../HStack';
+import { TableSearch } from '../TableSearch/TableSearch';
 import { TableHeaderButton } from './TableHeaderButton';
 import type { TableHeaderProps } from './TableHeaderProps';
 
-export const TableHeader = ({ children, testId = 'table-header' }: TableHeaderProps) => (
-  <HStack width="100%" alignVertical="center" alignHorizontal="end" spacing={8} data-testid={testId}>
-    {children}
-  </HStack>
-);
+export const TableHeader = ({ children, testId = 'table-header' }: TableHeaderProps) => {
+  const buttons: ReactElement[] = [];
+  const search: ReactElement[] = [];
+
+  const childArray = Children.toArray(children) as ReactElement[];
+
+  childArray.forEach(child => {
+    if (child.type === TableSearch) {
+      search.push(child);
+    }
+
+    if (child.type === TableHeaderButton) {
+      buttons.push(child);
+    }
+  });
+
+  return (
+    <HStack width="100%" alignVertical="center" spacing={8} data-testid={testId}>
+      {search}
+      {buttons.length > 0 && (
+        <HStack alignVertical="center" spacing={8} flexShrink={0}>
+          {buttons}
+        </HStack>
+      )}
+    </HStack>
+  );
+};
 
 TableHeader.displayName = 'TableHeader';
 
