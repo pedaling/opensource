86f9f4ca06d4388da6aa6eaa6a894cbcffabd58a
YimJiYoung <chiy323@gmail.com>
2023-05-16 20:00:38 +0900

    refactor: use useComposedRef in Box

--- packages/vibrant-core/src/lib/Box/Box.tsx
+++ packages/vibrant-core/src/lib/Box/Box.tsx
@@ -1,7 +1,7 @@
 import type { ComponentType, FC, ReactElement } from 'react';
-import { createElement, forwardRef, useCallback } from 'react';
+import { createElement, forwardRef } from 'react';
 import styled from '@emotion/styled';
-import { useLayoutEvent } from '@vibrant-ui/utils';
+import { useComposedRef, useLayoutEvent } from '@vibrant-ui/utils';
 import { OnColorContainer } from '../OnColorContainer';
 import type { BoxElements, BoxProps } from './BoxProps';
 import { interpolation, shouldForwardProp } from './BoxProps';
@@ -26,21 +26,7 @@ export const Box = styled(
       const Component = base ? (base as ComponentType<any>) : undefined;
 
       const { ref: layoutEventRef } = useLayoutEvent(onLayout);
-
-      const composeRef = useCallback(
-        (node: HTMLDivElement) => {
-          if (ref) {
-            if (typeof ref === 'function') {
-              ref(node);
-            } else {
-              ref.current = node;
-            }
-          }
-
-          layoutEventRef(node);
-        },
-        [ref, layoutEventRef]
-      );
+      const composeRef = useComposedRef(ref, layoutEventRef);
 
       if (Component) {
         return (
