87c10f4693354a84d362e7b831d8d11a84cdc9e8
Phillip(필립) <78997057+phillip-101@users.noreply.github.com>
2023-05-18 14:28:45 +0900

    feat: FormCardNumberField를 개발한다. [COM-4216] (#731)
    * feat: create form card number field
    
    * feat: create form card number field, card number field can get default value

--- packages/vibrant-components/src/index.ts
+++ packages/vibrant-components/src/index.ts
@@ -3,6 +3,7 @@ export { Backdrop } from './lib/Backdrop';
 export { BreadCrumbs } from './lib/BreadCrumbs';
 export { BreadCrumb } from './lib/BreadCrumb';
 export { CardNumberField } from './lib/CardNumberField';
+export type { CardNumberFieldProps } from './lib/CardNumberField';
 export { ContainedButton } from './lib/ContainedButton';
 export { ContentArea } from './lib/ContentArea';
 export { Display } from './lib/Display';
