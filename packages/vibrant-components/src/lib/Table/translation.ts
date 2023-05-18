221664719ddf1d16c18d05fb0148cfdead843f71
Maxi(맥시) <100735594+maxi-101@users.noreply.github.com>
2023-05-18 12:59:44 +0900

    fix: add text number of selected

---
+++ packages/vibrant-components/src/lib/Table/translation.ts
@@ -0,0 +1,13 @@
+import type { Translation } from '@vibrant-ui/core';
+
+export const ko: Translation['table'] = {
+  numberOfSelected: '{count}개 선택',
+};
+
+export const en: Translation['table'] = {
+  numberOfSelected: '{count} items selected',
+};
+
+export const ja: Translation['table'] = {
+  numberOfSelected: '{count} 個選択',
+};
