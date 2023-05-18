6c90292afc39be610f2a8af7a8f40e583dc4325d
Atom(아톰) <100661305+atom-class@users.noreply.github.com>
2023-05-18 22:21:26 +0900

    fix: set TableSearchOption storybook option args

--- packages/vibrant-components/src/lib/TableSearch/TableSearchOption/TableSearchOption.stories.tsx
+++ packages/vibrant-components/src/lib/TableSearch/TableSearchOption/TableSearchOption.stories.tsx
@@ -4,7 +4,15 @@ import { TableSearchOption } from './TableSearchOption';
 export default {
   title: 'TableSearchOption',
   component: TableSearchOption,
-  args: {},
+  args: {
+    options: [
+      { label: 'id', value: 'id' },
+      { label: 'title', value: 'title' },
+      { label: 'content', value: 'content' },
+      { label: 'author', value: 'author' },
+      { label: 'createdAt', value: 'createdAt' },
+    ],
+  },
 } as ComponentMeta<typeof TableSearchOption>;
 
 export const Basic: ComponentStory<typeof TableSearchOption> = props => <TableSearchOption {...props} />;
