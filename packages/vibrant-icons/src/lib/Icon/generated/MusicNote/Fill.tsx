270f805d7ccda9d317a413b512a85db67db65a5a
neroli-101 <132326695+neroli-101@users.noreply.github.com>
2023-05-24 14:44:00 +0900

    feat(icon): Add icons (#743)
    * feat(icon): add icons
    
    * fix: update snapshot

---
+++ packages/vibrant-icons/src/lib/Icon/generated/MusicNote/Fill.tsx
@@ -0,0 +1,15 @@
+import { Svg } from '@vibrant-ui/core';
+import type { IconComponent, IconProps } from '../../IconProp';
+
+export const Fill: IconComponent<IconProps, 'Fill'> = ({
+  size = 24,
+  fill = 'onColor',
+  testId = 'musicnote-fill',
+  ...props
+}) => (
+  <Svg viewBox="0 0 24 24" width={size} height={size} fill={fill} data-testid={testId} {...props}>
+    <Svg.Path d="M170 437c-45.22 0-82-36.78-82-82s36.78-82 82-82c13.25 0 26.32 3.23 38 9.35V72.29l151.68-24.27 2.32-.37V147.7l-110 17.6v189.69c0 45.22-36.78 82-82 82Z" />
+  </Svg>
+);
+
+Fill.iconType = 'Fill';
