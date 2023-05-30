95adabb502b872afa20a0a5ae395957ebd7d64e1
neroli-101 <132326695+neroli-101@users.noreply.github.com>
2023-05-30 21:57:29 +0900

    fix: Avatar placeholder 이미지를 svg로 교체한다 (#745)
    fix: change to svg

--- packages/vibrant-components/src/lib/Avatar/Avatar.stories.tsx
+++ packages/vibrant-components/src/lib/Avatar/Avatar.stories.tsx
@@ -7,7 +7,7 @@ export default {
   args: {
     size: 'lg',
     src: 'https://cdn.class101.net/images/7776e542-d6db-49c6-b86f-7e93f0cf5425/200x200.png',
-    placeholder: 'https://cdn.class101.net/images/e1cba897-73d1-43de-864b-c36cefdea670/200x200.png',
+    placeholder: 'https://cdn.class101.net/images/2483a7e5-8c20-4ac7-a209-805b4482664d',
     alt: 'avatar',
   },
 } as ComponentMeta<typeof Avatar>;
