8c21f734d1c508d0c93c60840637366dbfa1c697
ssong10 <logan@101.inc>
2023-06-20 10:51:03 +0900

    feat: gesturehandler rootview

---
+++ packages/vibrant-core/src/lib/AnimationProvider/AnimationProvider.tsx
@@ -0,0 +1,4 @@
+import type { FC } from 'react';
+import type { AnimationProviderProps } from './AnimationProviderProps';
+
+export const AnimationProvider: FC<AnimationProviderProps> = ({ children }) => <>{children}</>;
