0505e6faf91961561891bdbf4b88a038a3d3c3f7
kang-heewon <heewon.dev@gmail.com>
2023-06-22 20:32:45 +0900

    fix(vibrant-components): avatar가 네이티브에서 동작하지 않는 버그를 수정한다

--- packages/vibrant-components/src/lib/Avatar/AvatarProps.ts
+++ packages/vibrant-components/src/lib/Avatar/AvatarProps.ts
@@ -21,22 +21,23 @@ export const withAvatarVariation = withVariation<AvatarProps>('Avatar')(
       if (typeof size === 'number') {
         return {
           size,
+          borderRadius: size / 2,
         };
       }
 
       if (size === 'xs') {
-        return { size: 16 };
+        return { size: 16, borderRadius: 8 };
       }
 
       if (size === 'sm') {
-        return { size: 24 };
+        return { size: 24, borderRadius: 12 };
       }
 
       if (size === 'md') {
-        return { size: 30 };
+        return { size: 30, borderRadius: 15 };
       }
 
-      return { size: 44 };
+      return { size: 44, borderRadius: 22 };
     },
   })
 );
