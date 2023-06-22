87bac3300cace75c3adfed02e5fdb94c7c87751f
ssong10 <logan@101.inc>
2023-06-22 16:42:05 +0900

    fix: spinner color

--- packages/vibrant-components/src/lib/Spinner/Spinner.tsx
+++ packages/vibrant-components/src/lib/Spinner/Spinner.tsx
@@ -1,37 +1,45 @@
-import { Box } from '@vibrant-ui/core';
+import { Box, useCurrentTheme } from '@vibrant-ui/core';
 import { Motion } from '@vibrant-ui/motion';
 import { withSpinnerVariation } from './SpinnerProps';
 
-export const Spinner = withSpinnerVariation(({ borderWidth, borderRadius, testId = 'spinner', ...restProps }) => (
-  <Box as="span" data-testid={testId} {...restProps}>
-    <Box
-      as="span"
-      position="absolute"
-      top={0}
-      left={0}
-      right={0}
-      bottom={0}
-      borderStyle="solid"
-      borderWidth={borderWidth}
-      borderColor="onColor"
-      opacity={0.24}
-      borderRadius={borderRadius}
-    />
-    <Motion animation={{ rotate: { from: '0deg', to: '360deg' } }} duration={1400} loop={true} easing="linear">
+export const Spinner = withSpinnerVariation(({ borderWidth, borderRadius, testId = 'spinner', ...restProps }) => {
+  const {
+    theme: { colors },
+  } = useCurrentTheme();
+
+  const hexBorderColor = `${colors.onView1}3d`;
+
+  return (
+    <Box as="span" data-testid={testId} {...restProps}>
       <Box
         as="span"
         position="absolute"
-        transform={{ rotate: '360deg' }}
         top={0}
         left={0}
         right={0}
         bottom={0}
         borderStyle="solid"
         borderWidth={borderWidth}
-        borderColor="transparent"
-        borderTopColor="onColor"
+        borderColor="onColor"
+        opacity={0.24}
         borderRadius={borderRadius}
       />
-    </Motion>
-  </Box>
-));
+      <Motion animation={{ rotate: { from: '0deg', to: '360deg' } }} duration={1400} loop={true} easing="linear">
+        <Box
+          as="span"
+          position="absolute"
+          transform={{ rotate: '360deg' }}
+          top={0}
+          left={0}
+          right={0}
+          bottom={0}
+          borderStyle="solid"
+          borderWidth={borderWidth}
+          borderColor={hexBorderColor}
+          borderTopColor="onColor"
+          borderRadius={borderRadius}
+        />
+      </Motion>
+    </Box>
+  );
+});
