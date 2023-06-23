b2878f3892172c21642ddd66cb8a38262262dad1
ssong10 <logan@101.inc>
2023-06-23 14:19:59 +0900

    feat: delete unused box

--- packages/vibrant-components/src/lib/Spinner/Spinner.tsx
+++ packages/vibrant-components/src/lib/Spinner/Spinner.tsx
@@ -10,23 +10,23 @@ export const Spinner = withSpinnerVariation(({ borderWidth, borderRadius, testId
   const borderColorHex = `${onColorHex}3d`;
 
   return (
-    <Box as="span" data-testid={testId} {...restProps}>
-      <Motion animation={{ rotate: { from: '0deg', to: '360deg' } }} duration={1400} loop={true} easing="linear">
-        <Box
-          as="span"
-          position="absolute"
-          transform={{ rotate: '360deg' }}
-          top={0}
-          left={0}
-          right={0}
-          bottom={0}
-          borderStyle="solid"
-          borderWidth={borderWidth}
-          borderColor={borderColorHex}
-          borderTopColor="onColor"
-          borderRadius={borderRadius}
-        />
-      </Motion>
-    </Box>
+    <Motion animation={{ rotate: { from: '0deg', to: '360deg' } }} duration={1400} loop={true} easing="linear">
+      <Box
+        as="span"
+        position="absolute"
+        transform={{ rotate: '360deg' }}
+        top={0}
+        left={0}
+        right={0}
+        bottom={0}
+        borderStyle="solid"
+        borderWidth={borderWidth}
+        borderColor={borderColorHex}
+        borderTopColor="onColor"
+        borderRadius={borderRadius}
+        data-testid={testId}
+        {...restProps}
+      />
+    </Motion>
   );
 });
