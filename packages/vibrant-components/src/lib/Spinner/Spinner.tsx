e3a67b6a08ebae7e02b9583bbdd42659b2e82fac
ssong10 <logan@101.inc>
2023-06-23 13:44:35 +0900

    feat: spinner delete background box

--- packages/vibrant-components/src/lib/Spinner/Spinner.tsx
+++ packages/vibrant-components/src/lib/Spinner/Spinner.tsx
@@ -11,19 +11,6 @@ export const Spinner = withSpinnerVariation(({ borderWidth, borderRadius, testId
 
   return (
     <Box as="span" data-testid={testId} {...restProps}>
-      <Box
-        as="span"
-        position="absolute"
-        top={0}
-        left={0}
-        right={0}
-        bottom={0}
-        borderStyle="solid"
-        borderWidth={borderWidth}
-        borderColor="onColor"
-        opacity={0.24}
-        borderRadius={borderRadius}
-      />
       <Motion animation={{ rotate: { from: '0deg', to: '360deg' } }} duration={1400} loop={true} easing="linear">
         <Box
           as="span"
