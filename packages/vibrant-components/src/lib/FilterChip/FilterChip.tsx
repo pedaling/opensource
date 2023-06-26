4ea0cddd8d5cb663dccf3187d1c2923bf354cb5a
Mia (Jiyoung Yim) <mia@Mia-16.local>
2023-06-26 11:25:58 +0900

    refactor: remove inner view in FilterChip

--- packages/vibrant-components/src/lib/FilterChip/FilterChip.tsx
+++ packages/vibrant-components/src/lib/FilterChip/FilterChip.tsx
@@ -1,7 +1,6 @@
 import { cloneElement } from 'react';
 import { Box } from '@vibrant-ui/core';
 import { Body } from '../Body';
-import { HStack } from '../HStack';
 import { Pressable } from '../Pressable';
 import { withFilterChipVariation } from './FilterChipProps';
 
@@ -19,22 +18,29 @@ export const FilterChip = withFilterChipVariation(
     maxWidth = '100%',
     ...props
   }) => (
-    <Pressable testId={testId} flexGrow={0} flexShrink={0} borderRadiusLevel={5} maxWidth={maxWidth} {...props}>
-      <HStack as="span" my="auto" alignVertical="center">
-        {startIcon && (
-          <Box as="span" mr={spacing}>
-            {cloneElement(startIcon, { size: iconSize, fill: color })}
-          </Box>
-        )}
-        <Body level={bodyLevel} color={color} lineLimit={lineLimit} wordBreak={lineLimit ? 'break-all' : 'normal'}>
-          {children}
-        </Body>
-        {endIcon && (
-          <Box as="span" ml={spacing}>
-            {cloneElement(endIcon, { size: iconSize, fill: color })}
-          </Box>
-        )}
-      </HStack>
+    <Pressable
+      testId={testId}
+      flexGrow={0}
+      flexShrink={0}
+      borderRadiusLevel={5}
+      maxWidth={maxWidth}
+      flexDirection="row"
+      alignItems="center"
+      {...props}
+    >
+      {startIcon && (
+        <Box as="span" mr={spacing}>
+          {cloneElement(startIcon, { size: iconSize, fill: color })}
+        </Box>
+      )}
+      <Body level={bodyLevel} color={color} lineLimit={lineLimit} wordBreak={lineLimit ? 'break-all' : 'normal'}>
+        {children}
+      </Body>
+      {endIcon && (
+        <Box as="span" ml={spacing}>
+          {cloneElement(endIcon, { size: iconSize, fill: color })}
+        </Box>
+      )}
     </Pressable>
   )
 );
