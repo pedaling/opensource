7b7d0790328ba3b253baed62518493ccb591fd67
ssong10 <logan@101.inc>
2023-06-26 18:12:02 +0900

    fix: fix spinner position

--- packages/vibrant-components/src/lib/Spinner/Spinner.tsx
+++ packages/vibrant-components/src/lib/Spinner/Spinner.tsx
@@ -13,12 +13,7 @@ export const Spinner = withSpinnerVariation(({ borderWidth, borderRadius, testId
     <Motion animation={{ rotate: { from: '0deg', to: '360deg' } }} duration={1400} loop={true} easing="linear">
       <Box
         as="span"
-        position="absolute"
         transform={{ rotate: '360deg' }}
-        top={0}
-        left={0}
-        right={0}
-        bottom={0}
         borderStyle="solid"
         borderWidth={borderWidth}
         borderColor={borderColorHex}
