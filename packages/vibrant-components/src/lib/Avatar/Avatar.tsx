3262ec6959874417b491fb4d18a62967d052a07c
Ann (앤) <33626219+jhn3981@users.noreply.github.com>
2023-07-03 16:28:24 +0900

    fix: src가 빈문자열인 경우 바로 placeholder를 보여주게 수정한다 (#792)

--- packages/vibrant-components/src/lib/Avatar/Avatar.tsx
+++ packages/vibrant-components/src/lib/Avatar/Avatar.tsx
@@ -1,20 +1,27 @@
-import { useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import { Image } from '@vibrant-ui/core';
 import { useCustomization } from '../CustomizationProvider';
 import { withAvatarVariation } from './AvatarProps';
 
 export const Avatar = withAvatarVariation(({ testId, src, alt, size, borderRadius, placeholder }) => {
   const { avatar } = useCustomization();
-  const [fallbackSrc, setFallbackSrc] = useState<string>();
+
+  const fallbackSrc = useMemo(() => placeholder || avatar?.placeholder || '', [avatar, placeholder]);
+
+  const [imageSrc, setImageSrc] = useState<string>(src || fallbackSrc);
 
   const handleError = () => {
-    setFallbackSrc(placeholder ?? avatar?.placeholder);
+    setImageSrc(fallbackSrc);
   };
+
+  useEffect(() => {
+    setImageSrc(src || fallbackSrc);
+  }, [fallbackSrc, src]);
 
   return (
     <Image
       testId={testId}
-      src={fallbackSrc ?? src}
+      src={imageSrc}
       alt={alt}
       width={size}
       height={size}
