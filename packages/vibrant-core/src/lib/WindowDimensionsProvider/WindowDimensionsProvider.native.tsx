2a99f296cd0befde3e13709ac82094895672022c
peter <peter@101.inc>
2023-08-09 00:20:25 +0900

    fix: RN에서 useWindowDimension이 네이티브로 동작되게 한다

--- packages/vibrant-core/src/lib/WindowDimensionsProvider/WindowDimensionsProvider.native.tsx
+++ packages/vibrant-core/src/lib/WindowDimensionsProvider/WindowDimensionsProvider.native.tsx
@@ -1,39 +1,11 @@
 import type { FC, ReactElement } from 'react';
-import { createContext, useCallback, useContext, useMemo, useState } from 'react';
-import type { LayoutChangeEvent } from 'react-native';
-import { Dimensions, View } from 'react-native';
-import type { WindowDimensionsContextValue } from './WindowDimensionsProviderProps';
-
-const WindowDimensionsContext = createContext<WindowDimensionsContextValue>({
-  width: 0,
-  height: 0,
-});
+import { useWindowDimensions as useReactNativeWindowDimensions } from 'react-native';
 
 export const WindowDimensionsProvider: FC<{ children: ReactElement }> = ({ children }) => {
-  const [width, setWidth] = useState(() => Dimensions.get('window').width);
-  const [height, setHeight] = useState(() => Dimensions.get('window').height);
-
-  const onLayout = useCallback((event: LayoutChangeEvent) => {
-    setWidth(event.nativeEvent.layout.width);
-
-    setHeight(event.nativeEvent.layout.height);
-  }, []);
-
-  const contextValue = useMemo(
-    () => ({
-      width,
-      height,
-    }),
-    [height, width]
-  );
 
   return (
-    <WindowDimensionsContext.Provider value={contextValue}>
-      <View onLayout={onLayout} style={{ width: '100%', height: '100%' }}>
-        {children}
-      </View>
-    </WindowDimensionsContext.Provider>
+    children
   );
 };
 
-export const useWindowDimensions = () => useContext(WindowDimensionsContext);
+export const useWindowDimensions = () => useReactNativeWindowDimensions();
