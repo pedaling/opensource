d15aa25ea64e0f9c0869674a378dae18523394bc
peter <peter@101.inc>
2023-08-09 00:31:10 +0900

    fix: apply lint

--- packages/vibrant-core/src/lib/WindowDimensionsProvider/WindowDimensionsProvider.native.tsx
+++ packages/vibrant-core/src/lib/WindowDimensionsProvider/WindowDimensionsProvider.native.tsx
@@ -1,11 +1,6 @@
 import type { FC, ReactElement } from 'react';
 import { useWindowDimensions as useReactNativeWindowDimensions } from 'react-native';
 
-export const WindowDimensionsProvider: FC<{ children: ReactElement }> = ({ children }) => {
-
-  return (
-    children
-  );
-};
+export const WindowDimensionsProvider: FC<{ children: ReactElement }> = ({ children }) => children;
 
 export const useWindowDimensions = () => useReactNativeWindowDimensions();
