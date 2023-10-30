91adb07bb01c9ae7b937d6bd2112866d834ffede
wood <wood@101.inc>
2023-10-30 20:27:05 +0900

    fix: helper text can be react node

--- packages/vibrant-components/src/lib/TextField/TextFieldProps.ts
+++ packages/vibrant-components/src/lib/TextField/TextFieldProps.ts
@@ -1,4 +1,4 @@
-import type { ForwardedRef, ReactElement } from 'react';
+import type { ForwardedRef, ReactNode } from 'react';
 import type {
   AutoCapitalizeOption,
   AutoCompleteOption,
@@ -18,7 +18,7 @@ export type TextFieldProps = BaseInputProps<string> &
     state?: 'default' | 'error';
     label?: string;
     placeholder?: string;
-    helperText?: ReactElement | string;
+    helperText?: ReactNode | string;
     autoFocus?: boolean;
     maxLength?: number;
     autoComplete?: AutoCompleteOption;
