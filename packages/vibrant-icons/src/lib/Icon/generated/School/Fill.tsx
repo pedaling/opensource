91f7bced5d66ce41f8d6565a48d7e361fe338ee4
pott-101 <pott@101.inc>
2024-05-30 17:46:22 +0900

    feat: school, toteBagPlay 아이콘을 추가한다

---
+++ packages/vibrant-icons/src/lib/Icon/generated/School/Fill.tsx
@@ -0,0 +1,19 @@
+import { Svg } from '@vibrant-ui/core';
+import type { IconComponent, IconProps } from '../../IconProp';
+
+export const Fill: IconComponent<IconProps, 'Fill'> = ({
+  size = 24,
+  fill = 'onColor',
+  testId = 'school-fill',
+  ...props
+}) => (
+  <Svg viewBox="0 0 24 24" width={size} height={size} fill={fill} data-testid={testId} {...props}>
+    <Svg.Path
+      d="m17.65 9.5v.85h.85 3.4999v10.6504h-20v-10.6504h3.5001.85v-.85-3.87464l5.6499-3.47692 5.6501 3.47693zm-5.65 2.85c1.8502 0 3.35-1.4998 3.35-3.35 0-1.85015-1.4998-3.35-3.35-3.35s-3.35 1.49985-3.35 3.35c0 1.8502 1.4998 3.35 3.35 3.35z"
+      stroke="#0c0c0c"
+      strokeWidth={1.7}
+    />
+  </Svg>
+);
+
+Fill.iconType = 'Fill';
---
+++ packages/vibrant-icons/src/lib/Icon/generated/ToteBagPlay/Fill.tsx
@@ -0,0 +1,19 @@
+import { Svg } from '@vibrant-ui/core';
+import type { IconComponent, IconProps } from '../../IconProp';
+
+export const Fill: IconComponent<IconProps, 'Fill'> = ({
+  size = 24,
+  fill = 'onColor',
+  testId = 'totebagplay-fill',
+  ...props
+}) => (
+  <Svg viewBox="0 0 24 24" width={size} height={size} fill={fill} data-testid={testId} {...props}>
+    <Svg.Path
+      clipRule="evenodd"
+      d="m2.62547 21.391c.02133.2594.23806.459.49832.459h17.75231c.2603 0 .477-.1996.4983-.459l1.1963-14.54998c.024-.29132-.206-.54097-.4983-.54097h-5.4727v-1.02501c0-2.4675-2.0075-4.474991-4.475-4.474991h-.25c-2.46748 0-4.47498 2.007491-4.47498 4.474991v1.02501h-5.47268c-.29231 0-.52228.24966-.49832.54098zm11.77423-16.11596v1.02501h-4.79998v-1.02501c0-1.25449 1.02048-2.275 2.27498-2.275h.25c1.2545 0 2.275 1.02051 2.275 2.275zm1.6002 8.97496-6.49996-3.5.2 7z"
+      fillRule="evenodd"
+    />
+  </Svg>
+);
+
+Fill.iconType = 'Fill';
