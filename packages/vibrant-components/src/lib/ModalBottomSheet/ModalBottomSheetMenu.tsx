cabdfe795c979bd37316626c38544eb629cdef85
ssong10 <logan@101.inc>
2024-08-23 16:10:45 +0900

    fix: design

--- packages/vibrant-components/src/lib/ModalBottomSheet/ModalBottomSheetMenu.tsx
+++ packages/vibrant-components/src/lib/ModalBottomSheet/ModalBottomSheetMenu.tsx
@@ -1,16 +1,16 @@
 import { PressableBox } from '@vibrant-ui/core';
-import { Body } from '../Body';
 import { HStack } from '../HStack';
+import { Title } from '../Title';
 import { withModalBottomSheetMenuVariation } from './ModalBottomSheetMenuProps';
 
 export const ModalBottomSheetMenu = withModalBottomSheetMenuVariation(
   ({ rightContents, IconComponent, onClick, title, color }) => (
     <PressableBox onClick={onClick} flexDirection="row" alignItems="center" py={12}>
-      <HStack alignVertical="center" width="100%" spacing={8}>
+      <HStack alignVertical="center" width="100%" spacing={12}>
         {IconComponent && <IconComponent size={20} fill={color} />}
-        <Body level={1} color={color} flex={1}>
+        <Title level={6} color={color} flex={1} weight="regular">
           {title}
-        </Body>
+        </Title>
         {rightContents?.()}
       </HStack>
     </PressableBox>
