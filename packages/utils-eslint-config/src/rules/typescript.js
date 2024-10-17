5f97dddd03fcbae240838eeca8917be5b92226cb
Owen <heewon.dev@gmail.com>
2024-10-17 12:58:38 +0900

    feat: expression 간의 padding추가 룰을 제거한다

--- packages/utils-eslint-config/src/rules/typescript.js
+++ packages/utils-eslint-config/src/rules/typescript.js
@@ -24,7 +24,7 @@ const typescriptRules = {
   '@typescript-eslint/padding-line-between-statements': [
     'error',
     { blankLine: 'always', prev: '*', next: 'return' },
-    { blankLine: 'always', prev: ['expression'], next: ['expression'] },
+    { blankLine: 'always', prev: '*', next: 'block-like' },
     { blankLine: 'always', prev: ['if'], next: '*' },
     { blankLine: 'always', prev: ['const', 'let', 'var'], next: '*' },
     { blankLine: 'any', prev: ['const', 'let', 'var'], next: ['const', 'let', 'var'] },
