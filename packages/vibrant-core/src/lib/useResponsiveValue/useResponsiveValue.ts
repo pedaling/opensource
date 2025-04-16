9c08e80a2f33b079a46a7b309dd6a9d8c020949b
Joy <bae-unidev@users.noreply.github.com>
2025-04-16 14:10:59 +0900

    refactor: optimize component performance (#1019)

--- packages/vibrant-core/src/lib/useResponsiveValue/useResponsiveValue.ts
+++ packages/vibrant-core/src/lib/useResponsiveValue/useResponsiveValue.ts
@@ -1,61 +1,35 @@
-import { useCallback, useEffect, useState } from 'react';
+import { useCallback, useMemo } from 'react';
 import type { ResponsiveValue } from '../../types';
 import { useCurrentTheme } from '../ThemeProvider';
+import { useWindowDimensions } from '../WindowDimensionsProvider';
 import type { UseResponsiveValueProps } from './type';
 
-export const useResponsiveValue = ({ useRootBreakPoints = false, disabled = false }: UseResponsiveValueProps = {}) => {
-  const {
-    theme: { breakpoints },
-  } = useCurrentTheme({ root: useRootBreakPoints });
+export const useResponsiveValue = ({ useRootBreakPoints = false }: UseResponsiveValueProps = {}) => {
+  const { width } = useWindowDimensions();
 
-  const [currentIndex, setCurrentIndex] = useState(0);
+  const {
+    theme: { breakpoints: rootBreakpoint },
+  } = useCurrentTheme({ root: true });
+
+  const {
+    theme: { breakpoints: currentBreakpoint },
+  } = useCurrentTheme({ root: false });
+
+  const breakpointIndex = useMemo(() => {
+    const breakpoints = useRootBreakPoints ? rootBreakpoint : currentBreakpoint;
+
+    const index = breakpoints.findIndex(breakpoint => breakpoint >= width);
+
+    return index === -1 ? breakpoints.length : index;
+  }, [currentBreakpoint, rootBreakpoint, useRootBreakPoints, width]);
 
   const getResponsiveValue = useCallback(
     (responsiveValue: ResponsiveValue<any>) =>
       Array.isArray(responsiveValue)
-        ? responsiveValue[Math.min(currentIndex, responsiveValue.length - 1)]
+        ? responsiveValue[Math.min(breakpointIndex, responsiveValue.length - 1)]
         : responsiveValue,
-    [currentIndex]
+    [breakpointIndex]
   ) as <Value>(responsiveValue: ResponsiveValue<Value>) => Value;
 
-  useEffect(() => {
-    if (disabled) {
-      return;
-    }
-
-    const mediaQueryList = [window.matchMedia(`(max-width: ${breakpoints[0]}px)`)].concat(
-      breakpoints.map((breakpoint, index) =>
-        window.matchMedia(
-          `(min-width: ${breakpoint}px)${
-            breakpoints[index + 1] ? ` and (max-width: ${breakpoints[index + 1] - 1}px)` : ''
-          }`
-        )
-      )
-    );
-
-    const mediaQueryHandlers = mediaQueryList.map((mediaQuery, index) => {
-      const handler = (event: MediaQueryListEvent) => {
-        if (event.matches) {
-          setCurrentIndex(index);
-        }
-      };
-
-      if (mediaQuery.matches) {
-        setCurrentIndex(index);
-      }
-
-      mediaQuery.addListener(handler);
-
-      return handler;
-    });
-
-    return () => {
-      mediaQueryList.forEach((mediaQuery, index) => mediaQuery.removeListener(mediaQueryHandlers[index]));
-    };
-  }, [breakpoints, disabled]);
-
-  return {
-    getResponsiveValue,
-    breakpointIndex: currentIndex,
-  };
+  return { getResponsiveValue, breakpointIndex };
 };
