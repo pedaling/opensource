1eea00c92d509287578456299fc8a75e87257aa6
Owen <heewon.dev@gmail.com>
2025-09-19 12:24:42 +0900

    fix: graphql을 예외처리한다 (#1043)

--- packages/utils-eslint-config/src/rules/typescript.js
+++ packages/utils-eslint-config/src/rules/typescript.js
@@ -76,7 +76,7 @@ const typescriptRules = {
       selector: ['default', 'variable', 'typeLike', 'typeProperty'],
       format: null,
       filter: {
-        regex: '(VStack|HStack|ZStack|CSS|Pretendard|HTML|ZIndex|JSON)',
+        regex: '(VStack|HStack|ZStack|CSS|Pretendard|HTML|ZIndex|JSON|GraphQL|graphQLErrors)',
         match: true,
       },
     },
